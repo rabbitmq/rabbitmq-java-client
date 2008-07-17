d1c54c9dcd3f8aa6cdec4e9dc4dd981b3596e96a
Hubert Plociniczak <hubert@lshift.net>
2008-07-17 19:09:10 +0100

    jars are expected to be in the directory where the scripts are, not from where they are executed

--- scripts/runjava.sh
+++ scripts/runjava.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 RABBIT_JARS=
-SCRIPT_DIR=`dirname $0`
-for d in *.jar $SCRIPT_DIR/../build/lib/*.jar $SCIRPT_DIR/../lib/*.jar
+for d in `dirname $0`/*.jar
 do
     RABBIT_JARS="$d:$RABBIT_JARS"
 done
