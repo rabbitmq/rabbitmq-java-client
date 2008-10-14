79ad09ca89951d0209d1c946ddac67a52ce9c4cd
Matthias Radestock <matthias@lshift.net>
2008-10-14 14:37:44 +0100

    comment and indentation fixes

--- test/src/com/rabbitmq/client/test/functional/BindingLifecycle.java
+++ test/src/com/rabbitmq/client/test/functional/BindingLifecycle.java
@@ -10,14 +10,13 @@ import java.io.IOException;
 /**
  * This tests whether bindings are created and nuked properly.
  *
- * Provides test coverage for the report.
- *
- * TODO: Adjust this test when Queue.Unbind is implemented in the server
- *
+ * TODO: Adjust this test when Queue.Unbind is implemented in the
+ * server
  */
 public class BindingLifecycle extends PersisterRestartBase {
 
-    protected static final byte[] payload = (""+ System.currentTimeMillis()).getBytes();
+    protected static final byte[] payload =
+        (""+ System.currentTimeMillis()).getBytes();
 
     private static final int N = 1;
 
@@ -45,22 +44,21 @@ public class BindingLifecycle extends PersisterRestartBase {
     }
 
     /**
-     *
      * This tests whether the bindings attached to a durable exchange
      * are correctly blown away when the exhange is nuked.
      *
      * This complements a unit test for testing non-durable exhanges.
-     * In that case, an exchange is deleted and you expect any bindings
-     * hanging to it to be deleted as well. To verify this, the exchange
-     * is deleted and then recreated.
+     * In that case, an exchange is deleted and you expect any
+     * bindings hanging to it to be deleted as well. To verify this,
+     * the exchange is deleted and then recreated.
      *
-     * After the recreation, the old bindings should no longer exist and
-     * hence any messages published to that exchange get routed to /dev/null
+     * After the recreation, the old bindings should no longer exist
+     * and hence any messages published to that exchange get routed to
+     * /dev/null
      *
-     * This test exercises the durable variable of that test, so the main
-     * difference is that the broker has to be restarted to verify that
-     * the durable routes have been turfed.
-     *
+     * This test exercises the durable variable of that test, so the
+     * main difference is that the broker has to be restarted to
+     * verify that the durable routes have been turfed.
      */
     public void testDurableBindingsDeletion() throws Exception {
         declareDurableTopicExchange(X);
@@ -85,10 +83,11 @@ public class BindingLifecycle extends PersisterRestartBase {
 
 
     /**
-     * This tests whether the default bindings for persistent queues are recovered properly.
+     * This tests whether the default bindings for persistent queues
+     * are recovered properly.
      *
-     * The idea is to create a durable queue, nuke the server and then publish a
-     * message to it using the queue name as a routing key
+     * The idea is to create a durable queue, nuke the server and then
+     * publish a message to it using the queue name as a routing key
      */
     public void testDefaultBindingRecovery() throws Exception {
         declareDurableQueue(Q);
@@ -104,15 +103,19 @@ public class BindingLifecycle extends PersisterRestartBase {
     }
 
     /**
-     * This tests whether when you delete a queue, that its bindings are deleted as well.
+     * This tests whether when you delete a queue, that its bindings
+     * are deleted as well.
      */
     public void testQueueDelete() throws Exception {
 
         boolean durable = true;
         Binding binding = setupExchangeAndRouteMessage(durable);
 
-        // Nuke the queue and repeat this test, this time you expect nothing to get routed
-        // TODO: When unbind is implemented, use that instead of deleting and re-creating the queue
+        // Nuke the queue and repeat this test, this time you expect
+        // nothing to get routed.
+        //
+        // TODO: When unbind is implemented, use that instead of
+        // deleting and re-creating the queue
         channel.queueDelete(ticket, binding.q);
         channel.queueDeclare(ticket, binding.q, durable);
 
@@ -122,14 +125,16 @@ public class BindingLifecycle extends PersisterRestartBase {
     }
 
     /**
-     * This tests whether when you delete an exchange, that any bindings attached to it are deleted as well.
+     * This tests whether when you delete an exchange, that any
+     * bindings attached to it are deleted as well.
      */
     public void testExchangeDelete() throws Exception {
 
         boolean durable = true;
         Binding binding = setupExchangeAndRouteMessage(durable);
 
-        // Nuke the exchange and repeat this test, this time you expect nothing to get routed
+        // Nuke the exchange and repeat this test, this time you
+        // expect nothing to get routed
 
         channel.exchangeDelete(ticket, binding.x);
         channel.exchangeDeclare(ticket, binding.x, "direct");
@@ -140,10 +145,12 @@ public class BindingLifecycle extends PersisterRestartBase {
     }
 
     /**
-     * This tests whether the server checks that an exchange is actually being
-     * used when you try to delete it with the ifunused flag.
-     * To test this, you try to delete an exchange with a queue still bound to it
-     * and expect the delete operation to fail.
+     * This tests whether the server checks that an exchange is
+     * actually being used when you try to delete it with the ifunused
+     * flag.
+     *
+     * To test this, you try to delete an exchange with a queue still
+     * bound to it and expect the delete operation to fail.
      */
     public void testExchangeIfUnused() throws Exception {
 
@@ -163,47 +170,53 @@ public class BindingLifecycle extends PersisterRestartBase {
     }
 
     /**
-     * This tests whether the server checks that an auto_delete exchange
-     * actually deletes the bindings attached to it when it is deleted.
+     * This tests whether the server checks that an auto_delete
+     * exchange actually deletes the bindings attached to it when it
+     * is deleted.
      *
-     * To test this, you declare and auto_delete exchange and bind an auto_delete queue to it.
+     * To test this, you declare and auto_delete exchange and bind an
+     * auto_delete queue to it.
      *
-     * Start a consumer on this queue, send a message, let it get consumed and then cancel the consumer
+     * Start a consumer on this queue, send a message, let it get
+     * consumed and then cancel the consumer
      *
-     * The unsubscribe should cause the queue to auto_delete, which in turn should cause
-     * the exchange to auto_delete.
+     * The unsubscribe should cause the queue to auto_delete, which in
+     * turn should cause the exchange to auto_delete.
      *
      * Then re-declare the queue again and try to rebind it to the same exhange.
      *
-     * Because the exchange has been auto-deleted, the bind operation should fail.
+     * Because the exchange has been auto-deleted, the bind operation
+     * should fail.
      */
     public void testExchangeAutoDelete() throws Exception {
         doAutoDelete(false, 1);
     }
 
     /**
+     * Runs something similar to testExchangeAutoDelete, but adds
+     * different queues with the same binding to the same exchange.
      *
-     * Runs something similar to testExchangeAutoDelete, but adds different queues with
-     * the same binding to the same exchange.
-     *
-     * The difference should be that the original exchange should not get auto-deleted
-     *
+     * The difference should be that the original exchange should not
+     * get auto-deleted
      */
     public void testExchangeAutoDeleteManyBindings() throws Exception {
         doAutoDelete(false, 10);
     }
 
     /** 
-     * The same thing as testExchangeAutoDelete, but with durable queues.
+     * The same thing as testExchangeAutoDelete, but with durable
+     * queues.
      *
-     * Main difference is restarting the broker to make sure that the durable queues are blasted away.
+     * Main difference is restarting the broker to make sure that the
+     * durable queues are blasted away.
      */
     public void testExchangeAutoDeleteDurable() throws Exception {
         doAutoDelete(true, 1);
     }
 
     /**
-     * The same thing as testExchangeAutoDeleteManyBindings, but with durable queues.
+     * The same thing as testExchangeAutoDeleteManyBindings, but with
+     * durable queues.
      */
     public void testExchangeAutoDeleteDurableManyBindings() throws Exception {
         doAutoDelete(true, 10);
@@ -215,8 +228,10 @@ public class BindingLifecycle extends PersisterRestartBase {
 
         Binding binding = Binding.randomBinding();
 
-        channel.exchangeDeclare(ticket, binding.x, "direct", false, durable, true, null);
-        channel.queueDeclare(ticket, binding.q, false, durable, false, true, null);
+        channel.exchangeDeclare(ticket, binding.x, "direct",
+                                false, durable, true, null);
+        channel.queueDeclare(ticket, binding.q,
+                             false, durable, false, true, null);
         channel.queueBind(ticket, binding.q, binding.x, binding.k);
 
 
@@ -225,9 +240,12 @@ public class BindingLifecycle extends PersisterRestartBase {
             queueNames = new String[j];
             for (int i = 0 ; i < j ; i++) {
                 queueNames[i] = randomString();
-                channel.queueDeclare(ticket, queueNames[i], false, durable, false, false, null);
-                channel.queueBind(ticket, queueNames[i], binding.x, binding.k);
-                channel.basicConsume(ticket, queueNames[i], true, new QueueingConsumer(channel));
+                channel.queueDeclare(ticket, queueNames[i],
+                                     false, durable, false, false, null);
+                channel.queueBind(ticket, queueNames[i],
+                                  binding.x, binding.k);
+                channel.basicConsume(ticket, queueNames[i], true,
+                                     new QueueingConsumer(channel));
             }
         }
 
@@ -242,15 +260,18 @@ public class BindingLifecycle extends PersisterRestartBase {
         
         if (queues > 1) {
             for (String s : queueNames) {
-                channel.basicConsume(ticket, s, true, new QueueingConsumer(channel));
+                channel.basicConsume(ticket, s, true,
+                                     new QueueingConsumer(channel));
                 Binding tmp = new Binding(binding.x, s, binding.k);
                 sendUnroutable(tmp);
             }
         }
 
-        channel.queueDeclare(ticket, binding.q, false, durable, true, true, null);
+        channel.queueDeclare(ticket, binding.q,
+                             false, durable, true, true, null);
 
-        // if (queues == 1): Because the exchange does not exist, this bind should fail
+        // if (queues == 1): Because the exchange does not exist, this
+        // bind should fail
         try {
             channel.queueBind(ticket, binding.q, binding.x, binding.k);
             sendRoutable(binding);
@@ -277,21 +298,20 @@ public class BindingLifecycle extends PersisterRestartBase {
     }
 
     private void subscribeSendUnsubscribe(Binding binding) throws IOException {
-        String tag = channel.basicConsume(ticket, binding.q, new QueueingConsumer(channel));
+        String tag = channel.basicConsume(ticket, binding.q,
+                                          new QueueingConsumer(channel));
         sendUnroutable(binding);
         channel.basicCancel(tag);
     }
 
     private void sendUnroutable(Binding binding) throws IOException {
-        // Send it some junk
-        channel.basicPublish(ticket, binding.x, binding.k, MessageProperties.BASIC, payload);
+        channel.basicPublish(ticket, binding.x, binding.k, null, payload);
         GetResponse response = channel.basicGet(ticket, binding.q, true);
         assertNull("The response SHOULD BE null", response);
     }
 
     private void sendRoutable(Binding binding) throws IOException {
-        // Send it some junk
-        channel.basicPublish(ticket, binding.x, binding.k, MessageProperties.BASIC, payload);
+        channel.basicPublish(ticket, binding.x, binding.k, null, payload);
         GetResponse response = channel.basicGet(ticket, binding.q, true);
         assertNotNull("The response should not be null", response);
     }
@@ -315,26 +335,35 @@ public class BindingLifecycle extends PersisterRestartBase {
         }
     }
 
-    private void createQueueAndBindToExchange(Binding binding, boolean durable) throws Exception {
+    private void createQueueAndBindToExchange(Binding binding, boolean durable)
+        throws Exception {
+
         channel.exchangeDeclare(ticket, binding.x, "direct", durable);
         channel.queueDeclare(ticket, binding.q, durable);
         channel.queueBind(ticket, binding.q, binding.x, binding.k);
     }
 
-    private void deleteExchangeAndQueue(Binding binding) throws IOException {
+    private void deleteExchangeAndQueue(Binding binding)
+        throws IOException {
+
         channel.queueDelete(ticket, binding.q);
         channel.exchangeDelete(ticket, binding.x);
     }
 
-    private Binding setupExchangeBindings(boolean durable) throws Exception {
+    private Binding setupExchangeBindings(boolean durable)
+        throws Exception {
+
         Binding binding = Binding.randomBinding();
         createQueueAndBindToExchange(binding, durable);
         return binding;
     }
 
-    private Binding setupExchangeAndRouteMessage(boolean durable) throws Exception {
+    private Binding setupExchangeAndRouteMessage(boolean durable)
+        throws Exception {
+
         Binding binding = setupExchangeBindings(durable);
         sendRoutable(binding);
         return binding;
     }
+
 }
