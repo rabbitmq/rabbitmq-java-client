a420733ba1c409cb8e0cb80160e251c0677f7c2f
Matthias Radestock <matthias@lshift.net>
2008-10-14 13:57:00 +0100

    move BindingRestartTests into BindingTest

--- test/src/com/rabbitmq/client/test/functional/BindingTest.java
+++ test/src/com/rabbitmq/client/test/functional/BindingTest.java
@@ -15,9 +15,93 @@ import java.io.IOException;
  * TODO: Adjust this test when Queue.Unbind is implemented in the server
  *
  */
-public class BindingTest extends BrokerTestCase {
+public class BindingTest extends PersisterRestartBase {
 
     protected static final byte[] payload = (""+ System.currentTimeMillis()).getBytes();
+
+    private static final int N = 1;
+
+    protected static final String Q = "Q-" + System.currentTimeMillis();
+    protected static final String X = "X-" + System.currentTimeMillis();
+    protected static final String K = "K-" + System.currentTimeMillis();
+
+    /**
+     *   Tests whether durable bindings are correctly recovered.
+     */
+    public void testDurableBindingRecovery() throws Exception {
+        declareDurableTopicExchange(X);
+        declareAndBindDurableQueue(Q, X, K);
+
+        restart();
+
+        for (int i = 0; i < N; i++){
+            basicPublishVolatile(X, K);
+        }
+
+        assertDelivered(Q, N);
+
+        deleteQueue(Q);
+        deleteExchange(X);
+    }
+
+    /**
+     *
+     * This tests whether the bindings attached to a durable exchange
+     * are correctly blown away when the exhange is nuked.
+     *
+     * This complements a unit test for testing non-durable exhanges.
+     * In that case, an exchange is deleted and you expect any bindings
+     * hanging to it to be deleted as well. To verify this, the exchange
+     * is deleted and then recreated.
+     *
+     * After the recreation, the old bindings should no longer exist and
+     * hence any messages published to that exchange get routed to /dev/null
+     *
+     * This test exercises the durable variable of that test, so the main
+     * difference is that the broker has to be restarted to verify that
+     * the durable routes have been turfed.
+     *
+     */
+    public void testDurableBindingsDeletion() throws Exception {
+        declareDurableTopicExchange(X);
+        declareAndBindDurableQueue(Q, X, K);
+
+        deleteExchange(X);
+
+        restart();
+
+        declareDurableTopicExchange(X);
+
+        for (int i = 0; i < N; i++){
+            basicPublishVolatile(X, K);
+        }
+
+        GetResponse response = channel.basicGet(ticket, Q, true);
+        assertNull("The initial response SHOULD BE null", response);
+
+        deleteQueue(Q);
+        deleteExchange(X);
+    }
+
+
+    /**
+     * This tests whether the default bindings for persistent queues are recovered properly.
+     *
+     * The idea is to create a durable queue, nuke the server and then publish a
+     * message to it using the queue name as a routing key
+     */
+    public void testDefaultBindingRecovery() throws Exception {
+        declareDurableQueue(Q);
+
+        restart();
+
+        basicPublishVolatile("", Q);
+
+        GetResponse response = channel.basicGet(ticket, Q, true);
+        assertNotNull("The initial response SHOULD NOT be null", response);
+
+        deleteQueue(Q);
+    }
 
     /**
      * This tests whether when you delete a queue, that its bindings are deleted as well.
