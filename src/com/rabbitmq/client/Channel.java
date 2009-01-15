131bb99ccb17affeb0ff7203f057901ef7be9d31
Matthias Radestock <matthias@lshift.net>
2009-01-15 16:27:26 +0000

    correct typo

--- src/com/rabbitmq/client/Channel.java
+++ src/com/rabbitmq/client/Channel.java
@@ -124,7 +124,7 @@ public interface Channel extends ShutdownNotifier{
      * Request specific "quality of service" settings.
      *
      * These settings impose limits on the amount of data the server
-     * will deliver to consumers before requireing the receipt of
+     * will deliver to consumers before requiring the receipt of
      * acknowledgements.
      * Thus they provide a means of consumer-initiated flow control.
      * @see com.rabbitmq.client.AMQP.Basic.Qos
