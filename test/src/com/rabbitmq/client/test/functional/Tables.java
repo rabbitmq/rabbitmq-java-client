0d2acd3b9e00dd202e8bf7f516c5c168d6af97fb
Matthias Radestock <matthias@lshift.net>
2009-04-28 12:32:13 +0100

    test table transmission as part of method arguments

--- test/src/com/rabbitmq/client/test/functional/Tables.java
+++ test/src/com/rabbitmq/client/test/functional/Tables.java
@@ -47,33 +47,38 @@ public class Tables extends BrokerTestCase
 
     public void testTypes() throws IOException {
 
-        AMQP.Queue.DeclareOk ok = channel.queueDeclare();
-        String q = ok.getQueue();
-
-        Map<String, Object> headers = new HashMap<String, Object>();
+        Map<String, Object> table = new HashMap<String, Object>();
         Map<String, Object> subTable = new HashMap<String, Object>();
         subTable.put("key", 1);
-        headers.put("S", LongStringHelper.asLongString("string"));
-        headers.put("I", new Integer(1));
-        headers.put("D", new BigDecimal("1.1"));
-        headers.put("T", new java.util.Date(1000000));
-        headers.put("F", subTable);
-        headers.put("b", (byte)1);
-        headers.put("d", 1.1d);
-        headers.put("f", 1.1f);
-        headers.put("l", 1L);
-        headers.put("s", (short)1);
-        headers.put("t", true);
-        headers.put("x", "byte".getBytes());
-        headers.put("V", null);
-        BasicProperties props = new BasicProperties(null, null, headers, null,
+        table.put("S", LongStringHelper.asLongString("string"));
+        table.put("I", new Integer(1));
+        table.put("D", new BigDecimal("1.1"));
+        table.put("T", new java.util.Date(1000000));
+        table.put("F", subTable);
+        table.put("b", (byte)1);
+        table.put("d", 1.1d);
+        table.put("f", 1.1f);
+        table.put("l", 1L);
+        table.put("s", (short)1);
+        table.put("t", true);
+        table.put("x", "byte".getBytes());
+        table.put("V", null);
+
+        //roundtrip of content headers
+        AMQP.Queue.DeclareOk ok = channel.queueDeclare();
+        String q = ok.getQueue();
+        BasicProperties props = new BasicProperties(null, null, table, null,
                                                     null, null, null, null,
                                                     null, null, null, null,
                                                     null, null);
-
         channel.basicPublish("", q, props, "".getBytes());
         BasicProperties rProps = channel.basicGet(q, true).getProps();
         assertMapsEqual(props.headers, rProps.headers);
+
+        //sending as part of method arguments - we are relying on
+        //exchange.declare ignoring the arguments table.
+        channel.exchangeDeclare("x", "direct", false, false, false, table);
+        channel.exchangeDelete("x");
     }
 
     private static void assertMapsEqual(Map<String, Object> a,
