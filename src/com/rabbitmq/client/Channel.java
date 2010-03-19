28755543773e53cba09d23e1ebb92714d9641315
Matthew Sackman <matthew@lshift.net>
2010-03-19 14:43:25 +0000

    Incorrect import

--- src/com/rabbitmq/client/Channel.java
+++ src/com/rabbitmq/client/Channel.java
@@ -37,7 +37,7 @@ import com.rabbitmq.client.AMQP.BasicProperties;
 import com.rabbitmq.client.AMQP.Exchange;
 import com.rabbitmq.client.AMQP.Queue;
 import com.rabbitmq.client.AMQP.Tx;
-import com.rabbitmq.client.impl.AMQImpl.Channel.FlowOk;
+import com.rabbitmq.client.AMQP.Channel.FlowOk;
 
 /**
  * Public API: Interface to an AMQ channel. See the <a href="http://www.amqp.org/">spec</a> for details.
