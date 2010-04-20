fe622de4218961828769e28403e80b4f8984d577
Matthew Sackman <matthew@lshift.net>
2010-04-20 21:22:46 +0100

    utf8 stuff is unnecessary

--- test/src/com/rabbitmq/client/test/server/PersisterRestart6.java
+++ test/src/com/rabbitmq/client/test/server/PersisterRestart6.java
@@ -4,22 +4,21 @@ import java.io.IOException;
 
 public class PersisterRestart6 extends RestartBase {
 
-    private static final String utf8 = "UTF-8";
     private static final String q = "Restart6";
     
     public void testRestart() throws IOException, InterruptedException {
         declareDurableQueue(q);
-        basicPublishPersistent("a".getBytes(utf8), q);
-        basicPublishPersistent("b".getBytes(utf8), q);
-        basicPublishPersistent("c".getBytes(utf8), q);
+        basicPublishPersistent("a".getBytes(), q);
+        basicPublishPersistent("b".getBytes(), q);
+        basicPublishPersistent("c".getBytes(), q);
         forceSnapshot();
         restart();
-        assertTrue(new String(basicGet(q).getBody(), utf8).equals("a"));
+        assertTrue(new String(basicGet(q).getBody()).equals("a"));
         forceSnapshot();
         restart();
         restart();
-        assertTrue(new String(basicGet(q).getBody(), utf8).equals("b"));
-        assertTrue(new String(basicGet(q).getBody(), utf8).equals("c"));
+        assertTrue(new String(basicGet(q).getBody()).equals("b"));
+        assertTrue(new String(basicGet(q).getBody()).equals("c"));
         deleteQueue(q);
     }
     
