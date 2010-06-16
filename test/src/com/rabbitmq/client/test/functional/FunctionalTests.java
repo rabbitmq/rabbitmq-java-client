ff879474bf1f8bf2b4ac9097b27836be033cde72
Simon MacMullen <simon@rabbitmq.com>
2010-06-16 16:24:12 +0100

    Add a test to make what we already do apparent.

--- test/src/com/rabbitmq/client/test/functional/FunctionalTests.java
+++ test/src/com/rabbitmq/client/test/functional/FunctionalTests.java
@@ -58,6 +58,7 @@ public class FunctionalTests extends TestCase {
         suite.addTestSuite(QueueExclusivity.class);
         suite.addTestSuite(InvalidAcks.class);
         suite.addTestSuite(InvalidAcksTx.class);
+        suite.addTestSuite(BindToDefaultExchange.class);
         return suite;
     }
 }
