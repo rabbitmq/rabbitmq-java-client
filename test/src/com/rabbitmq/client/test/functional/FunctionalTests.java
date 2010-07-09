2c6b25e2c1d490688ab8165362503527a799eee7
Emile Joubert <emile@rabbitmq.com>
2010-07-09 15:41:57 +0100

    Tests for 0-9-1 error codes

--- test/src/com/rabbitmq/client/test/functional/FunctionalTests.java
+++ test/src/com/rabbitmq/client/test/functional/FunctionalTests.java
@@ -63,6 +63,7 @@ public class FunctionalTests extends TestCase {
         suite.addTestSuite(BindToDefaultExchange.class);
         suite.addTestSuite(UnbindAutoDeleteExchange.class);
         suite.addTestSuite(RecoverAfterCancel.class);
+        suite.addTestSuite(UnexpectedFrames.class);
         return suite;
     }
 }
