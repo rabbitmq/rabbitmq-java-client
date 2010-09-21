95cbe951406039d2445b7931f9a488218d5c5d3b
Rob Harrop <rharrop@vmware.com>
2010-09-21 15:39:36 +0100

    added queue ttl to the functional test list

--- test/src/com/rabbitmq/client/test/functional/FunctionalTests.java
+++ test/src/com/rabbitmq/client/test/functional/FunctionalTests.java
@@ -67,6 +67,7 @@ public class FunctionalTests extends TestCase {
         suite.addTestSuite(UnbindAutoDeleteExchange.class);
         suite.addTestSuite(RecoverAfterCancel.class);
         suite.addTestSuite(UnexpectedFrames.class);
+        suite.addTestSuite(PerQueueTTL.class);
         return suite;
     }
 }
