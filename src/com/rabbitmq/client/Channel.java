3cf26e511ab99b52832e75c6974603a0c40981ad
Jerry Kuch <jerryk@vmware.com>
2010-11-11 18:43:20 +0000

    Set up exchanges, queues and bindings for internal exchange test.

--- src/com/rabbitmq/client/Channel.java
+++ src/com/rabbitmq/client/Channel.java
@@ -39,6 +39,7 @@ import com.rabbitmq.client.AMQP.Queue;
 import com.rabbitmq.client.AMQP.Tx;
 import com.rabbitmq.client.AMQP.Basic;
 import com.rabbitmq.client.AMQP.Channel.FlowOk;
+import com.rabbitmq.client.impl.AMQImpl;
 
 /**
  * Public API: Interface to an AMQ channel. See the <a href="http://www.amqp.org/">spec</a> for details.
@@ -271,6 +272,29 @@ public interface Channel extends ShutdownNotifier {
      */
     Exchange.DeclareOk exchangeDeclare(String exchange, String type, boolean durable, boolean autoDelete,
                                        Map<String, Object> arguments) throws IOException;
+
+    /**
+     * Declare an exchange, via an interface that allows the complete set of
+     * arguments.
+     * @see com.rabbitmq.client.AMQP.Exchange.Declare
+     * @see com.rabbitmq.client.AMQP.Exchange.DeclareOk
+     * @param exchange the name of the exchange
+     * @param type the exchange type
+     * @param durable true if we are declaring a durable exchange (the exchange will survive a server restart)
+     * @param autoDelete true if the server should delete the exchange when it is no longer in use
+     * @param internal true if the exchange is internal, i.e. can't be directly
+     * published to by a client.
+     * @param arguments other properties (construction arguments) for the exchange
+     * @return a declaration-confirm method to indicate the exchange was successfully declared
+     * @throws java.io.IOException if an error is encountered
+     */
+    public AMQImpl.Exchange.DeclareOk exchangeDeclare(String exchange,
+                                                      String type,
+                                                      boolean durable,
+                                                      boolean autoDelete,
+                                                      boolean internal,
+                                                      Map<String, Object> arguments)
+            throws IOException;
 
     /**
      * Declare an exchange passively; that is, check if the named exchange exists.
