65f39e1471dce55f63849c8b3f3ac0604d7ea667
Matthias Radestock <matthias@rabbitmq.com>
2011-01-12 18:30:08 +0000

    cosmetics and minor refactoring eliminate some helper methods better naming

--- test/src/com/rabbitmq/examples/MulticastMain.java
+++ test/src/com/rabbitmq/examples/MulticastMain.java
@@ -242,7 +242,7 @@ public class MulticastMain {
         private long    startTime;
         private long    lastStatsTime;
         private int     msgCount;
-        private int     basicReturnCount;
+        private int     returnCount;
 
         private boolean confirm;
         private long    confirmMax;
@@ -270,33 +270,25 @@ public class MulticastMain {
             this.confirm      = confirm;
         }
 
-        public void handleBasicReturn(int replyCode,
-                                      String replyText,
-                                      String exchange,
-                                      String routingKey,
-                                      AMQP.BasicProperties properties,
-                                      byte[] body) throws IOException {
-            logBasicReturn();
+        public synchronized void handleBasicReturn(int replyCode,
+                                                   String replyText,
+                                                   String exchange,
+                                                   String routingKey,
+                                                   AMQP.BasicProperties properties,
+                                                   byte[] body)
+            throws IOException {
+            returnCount++;
         }
 
-        public synchronized void logBasicReturn() {
-            basicReturnCount++;
-        }
-
-        public synchronized void resetBasicReturns() {
-            basicReturnCount = 0;
-        }
-
-        public void handleAck(long sequenceNumber, boolean multiple) {
-            logAck(sequenceNumber);
-        }
-
-        private synchronized void resetConfirms() {
+        public synchronized void resetCounts() {
+            msgCount = 0;
+            returnCount = 0;
             confirmCount = 0;
         }
 
-        private synchronized void logAck(long seqNum) {
-            mostRecentConfirmed = seqNum;
+        public synchronized void handleAck(long sequenceNumber,
+                                           boolean multiple) {
+            mostRecentConfirmed = sequenceNumber;
             confirmCount++;
         }
 
@@ -368,8 +360,8 @@ public class MulticastMain {
                                  (msgCount * 1000L / elapsed) +
                                  " msg/s");
                 if (mandatory || immediate) {
-                    System.out.print(", basic returns: " +
-                                     (basicReturnCount * 1000L / elapsed) +
+                    System.out.print(", returns: " +
+                                     (returnCount * 1000L / elapsed) +
                                      " ret/s");
                 }
                 if (confirm) {
@@ -378,9 +370,7 @@ public class MulticastMain {
                                      " c/s");
                 }
                 System.out.println();
-                resetBasicReturns();
-                resetConfirms();
-                msgCount = 0;
+                resetCounts();
                 lastStatsTime = now;
             }
         }
