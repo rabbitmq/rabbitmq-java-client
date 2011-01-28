b3e0036dfcc43cbf5eb9f4965bc60f66b5dc2b27
Emile Joubert <emile@rabbitmq.com>
2011-01-28 13:30:03 +0000

    Support Java 1.5

--- src/com/rabbitmq/client/PossibleAuthenticationFailureException.java
+++ src/com/rabbitmq/client/PossibleAuthenticationFailureException.java
@@ -1,6 +1,6 @@
 //  The contents of this file are subject to the Mozilla Public License
 //  Version 1.1 (the "License"); you may not use this file except in
-//  compliance with the License. You may obtain a copy of the License
+//  compliance with the License. You may obtain IOa copy of the License
 //  at http://www.mozilla.org/MPL/
 //
 //  Software distributed under the License is distributed on an "AS IS"
@@ -25,7 +25,7 @@ public class PossibleAuthenticationFailureException extends IOException
 {
     public PossibleAuthenticationFailureException(Throwable cause)
     {
-        super("Possibly caused by authentication failure",
-              cause);
+        super("Possibly caused by authentication failure");
+        super.initCause(cause);
     }
 }
