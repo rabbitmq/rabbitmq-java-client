e6f6c69307b0aefecb7c5aca66566088895d81d8
Emile Joubert <emile@rabbitmq.com>
2011-02-14 16:55:03 +0000

    Improved CcRoutes tests
    - Use generics
    - Check that headers which should remain intact are
    - Use auto-delete and exclusive queues and exchanges

--- test/src/com/rabbitmq/client/test/functional/CcRoutes.java
+++ test/src/com/rabbitmq/client/test/functional/CcRoutes.java
@@ -33,74 +33,65 @@ public class CcRoutes extends BrokerTestCase  {
     protected String exDirect = "direct_cc_exchange";
     protected String exTopic = "topic_cc_exchange";
     protected BasicProperties props;
-    protected Map headers;
-    protected List ccList;
-    protected List bccList;
+    protected Map<String, Object> headers;
+    protected List<String> ccList;
+    protected List<String> bccList;
 
     @Override protected void setUp() throws IOException {
         super.setUp();
         props = new BasicProperties();
-        headers = new HashMap();
-        ccList = new ArrayList();
-        bccList = new ArrayList();
+        headers = new HashMap<String, Object>();
+        ccList = new ArrayList<String>();
+        bccList = new ArrayList<String>();
     }
 
     @Override protected void createResources() throws IOException {
         super.createResources();
         for (String q : queues) {
-            channel.queueDeclare(q, false, false, false, null);
+            channel.queueDeclare(q, false, true, true, null);
         }
-        channel.exchangeDeclare(exDirect, "direct");
-        channel.exchangeDeclare(exTopic, "topic");
-    }
-
-    @Override protected void releaseResources() throws IOException {
-        for (String q : queues) {
-            channel.queueDelete(q);
-        }
-        channel.exchangeDelete(exDirect);
-        channel.exchangeDelete(exTopic);
-        super.releaseResources();
+        channel.exchangeDeclare(exDirect, "direct", false, true, null);
+        channel.exchangeDeclare(exTopic, "topic", false, true, null);
     }
 
     public void testCcList() throws IOException {
         ccList.add("queue2");
         ccList.add("queue3");
         headerPublish("", "queue1", ccList, null);
-        expect(new String []{"queue1", "queue2", "queue3"});
+        expect(new String []{"queue1", "queue2", "queue3"}, true);
      }
 
     public void testCcIgnoreEmptyAndInvalidRoutes() throws IOException {
         bccList.add("frob");
         headerPublish("", "queue1", ccList, bccList);
-        expect(new String []{"queue1"});
+        expect(new String []{"queue1"}, true);
      }
 
     public void testBcc() throws IOException {
         bccList.add("queue2");
         headerPublish("", "queue1", null, bccList);
-        expect(new String []{"queue1", "queue2"});
+        expect(new String []{"queue1", "queue2"}, false);
      }
 
     public void testNoDuplicates() throws IOException {
         ccList.add("queue1");
         ccList.add("queue1");
         bccList.add("queue1");
-        headerPublish("", "queue1", null, bccList);
-        expect(new String[] {"queue1"});
+        headerPublish("", "queue1", ccList, bccList);
+        expect(new String[] {"queue1"}, true);
      }
 
     public void testDirectExchangeWithoutBindings() throws IOException {
         ccList.add("queue1");
         headerPublish(exDirect, "queue2", ccList, null);
-        expect(new String[] {});
+        expect(new String[] {}, true);
     }
 
     public void testTopicExchange() throws IOException {
         ccList.add("routing_key");
         channel.queueBind("queue2", exTopic, "routing_key");
         headerPublish(exTopic, "", ccList, null);
-        expect(new String[] {"queue2"});
+        expect(new String[] {"queue2"}, true);
     }
 
     public void testBoundExchanges() throws IOException {
@@ -109,10 +100,10 @@ public class CcRoutes extends BrokerTestCase  {
         channel.exchangeBind(exTopic, exDirect, "routing_key1");
         channel.queueBind("queue2", exTopic, "routing_key2");
         headerPublish(exDirect, "", ccList, bccList);
-        expect(new String[] {"queue2"});
+        expect(new String[] {"queue2"}, true);
     }
 
-    private void headerPublish(String ex, String to, List cc, List bcc) throws IOException {
+    private void headerPublish(String ex, String to, List<String> cc, List<String> bcc) throws IOException {
         if (cc != null) {
             headers.put("CC", ccList);
         }
@@ -123,17 +114,19 @@ public class CcRoutes extends BrokerTestCase  {
         channel.basicPublish(ex, to, props, new byte[0]);
     }
 
-    private void expect(String[] expectedQueues) throws IOException {
+    private void expect(String[] expectedQueues, boolean usedCc) throws IOException {
         GetResponse getResponse;
-        List expectedList = Arrays.asList(expectedQueues);
+        List<String> expectedList = Arrays.asList(expectedQueues);
         for (String q : queues) {
             getResponse = basicGet(q);
             if (expectedList.contains(q)) {
                 assertNotNull(getResponse);
                 assertEquals(0, getResponse.getMessageCount());
                 Map headers = getResponse.getProps().getHeaders();
-                if (headers != null)
+                if (headers != null){
+                    assertEquals(usedCc, headers.containsKey("CC"));
                     assertFalse(headers.containsKey("BCC"));
+                }
             } else {
                 assertNull(getResponse);
             }
