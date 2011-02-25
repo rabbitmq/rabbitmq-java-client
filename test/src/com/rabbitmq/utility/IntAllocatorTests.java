21e91c173bdd949cd3f10ba824be782d2929a850
Steve Powell <steve@rabbitmq.com>
2011-02-25 15:37:29 +0000

    Added IntBitSetAllocator.java and IntAllocatorTests tests it.

--- test/src/com/rabbitmq/utility/IntAllocatorTests.java
+++ test/src/com/rabbitmq/utility/IntAllocatorTests.java
@@ -28,51 +28,132 @@ public class IntAllocatorTests extends TestCase {
     private static final int TEST_ITERATIONS = 50000;
     private static final int HI_RANGE = 100000;
     private static final int LO_RANGE = 100;
-    private IntAllocator intAllocator = new IntAllocator(LO_RANGE, HI_RANGE);
+    private IntAllocator iAll = new IntAllocator(LO_RANGE, HI_RANGE);
 
-    private Random rand = new Random();
+    private Random rand = new Random(70608L);
 
     public void testReserveAndFree() throws Exception {
         Set<Integer> set = new HashSet<Integer>();
         for (int i = 0; i < TEST_ITERATIONS; ++i) {
-            int trial = getTrial();
+            int trial = getTrial(rand);
             if (set.contains(trial)) {
-                intAllocator.free(trial);
+                iAll.free(trial);
                 set.remove(trial);
             } else {
                 assertTrue("Did not reserve free integer " + trial,
-                        intAllocator.reserve(trial));
+                        iAll.reserve(trial));
                 set.add(trial);
             }
         }
 
         for (int trial : set) {
             assertFalse("Integer " + trial + " not allocated!",
-                    intAllocator.reserve(trial));
+                    iAll.reserve(trial));
         }
     }
 
     public void testAllocateAndFree() throws Exception {
         Set<Integer> set = new HashSet<Integer>();
         for (int i=0; i < TEST_ITERATIONS; ++i) {
-            if (getBool()) {
-                int trial = intAllocator.allocate();
+            if (getBool(rand)) {
+                int trial = iAll.allocate();
                 assertFalse("Already allocated " + trial, set.contains(trial));
                 set.add(trial);
             } else {
                 if (!set.isEmpty()) {
                     int trial = extract(set);
                     assertFalse("Allocator agreed to reserve " + trial,
-                            intAllocator.reserve(trial));
-                    intAllocator.free(trial);
+                            iAll.reserve(trial));
+                    iAll.free(trial);
                 }
             }
         }
 
         for (int trial : set) {
             assertFalse("Integer " + trial + " should be allocated!",
-                    intAllocator.reserve(trial));
+                    iAll.reserve(trial));
         }
+    }
+
+    public void testReserveAndFreeBS() throws Exception {
+        IntBitSetAllocator ibs = new IntBitSetAllocator(LO_RANGE, HI_RANGE);
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < TEST_ITERATIONS; ++i) {
+            int trial = getTrial(rand);
+            if (set.contains(trial)) {
+                ibs.free(trial);
+                set.remove(trial);
+            } else {
+                assertTrue("Did not reserve free integer " + trial,
+                        ibs.reserve(trial));
+                set.add(trial);
+            }
+        }
+
+        for (int trial : set) {
+            assertFalse("Integer " + trial + " not allocated!",
+                    ibs.reserve(trial));
+        }
+    }
+
+    public void testAllocateAndFreeBS() throws Exception {
+        IntBitSetAllocator ibs = new IntBitSetAllocator(LO_RANGE, HI_RANGE);
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i=0; i < TEST_ITERATIONS; ++i) {
+            if (getBool(rand)) {
+                int trial = ibs.allocate();
+                assertFalse("Already allocated " + trial, set.contains(trial));
+                set.add(trial);
+            } else {
+                if (!set.isEmpty()) {
+                    int trial = extract(set);
+                    assertFalse("Allocator agreed to reserve " + trial,
+                            ibs.reserve(trial));
+                    ibs.free(trial);
+                }
+            }
+        }
+
+        for (int trial : set) {
+            assertFalse("Integer " + trial + " should be allocated!",
+                    ibs.reserve(trial));
+        }
+    }
+
+    public void testToStringBS() throws Exception {
+        IntBitSetAllocator ibs = new IntBitSetAllocator(LO_RANGE, HI_RANGE);
+        assertEquals("IntBitSetAllocator{allocated = []}", ibs.toString());
+        ibs.allocate();
+        assertEquals("IntBitSetAllocator{allocated = [100]}", ibs.toString());
+        for(int i = 200; i<211; i=i+4) {
+            ibs.reserve(i);
+            ibs.reserve(i+1);
+            ibs.reserve(i+2);
+        }
+        assertEquals("IntBitSetAllocator{allocated = "
+                    + "[100, 200..202, 204..206, 208..210]}"
+                    , ibs.toString());
+    }
+
+    public void testToString() throws Exception {
+        assertEquals("IntAllocator{intervals = [100..100000], unsorted = []}"
+                    , iAll.toString());
+        iAll.allocate();
+        assertEquals("IntAllocator{intervals = [101..100000], unsorted = []}"
+                    , iAll.toString());
+        iAll.free(100);
+        assertEquals("IntAllocator{intervals = [101..100000]"
+                    + ", unsorted = [100]}"
+                    , iAll.toString());
+        for(int i = 200; i<211; i=i+4) {
+            iAll.reserve(i);
+            iAll.reserve(i+1);
+            iAll.reserve(i+2);
+        }
+        assertEquals("IntAllocator{intervals = "
+                    + "[100..199, 203..203, 207..207, 211..100000]"
+                    + ", unsorted = []}"
+                    , iAll.toString());
     }
 
     private static int extract(Set<Integer> set) {
@@ -82,11 +163,11 @@ public class IntAllocatorTests extends TestCase {
         return trial;
     }
     
-    private int getTrial() {
+    private static int getTrial(Random rand) {
         return rand.nextInt(HI_RANGE-LO_RANGE+1) + LO_RANGE;
     }
     
-    private boolean getBool() {
+    private static boolean getBool(Random rand) {
         return rand.nextBoolean();
     }
 }
