8f291a8a388879a5caefbc16fbb8bd061b120069
Matthias Radestock <matthias@rabbitmq.com>
2011-06-25 18:01:28 +0100

    rewrite EffectVisibilityCrossNodeTest to not use tx The test originated with bug 19844, which in turn was a follow-on from bug 19758. The issue we discovered in the latter was that queue.* and tx.* operations could overtake publishes on their way to queues. The test checked (probabilistically) that this wasn't happening for tx, which is the most prominent and dangerous manifestation.
    With tx gone we need to switch to testing another operation. I chose
    queue.purge.
    
    I also switched to using "amq.fanout" rather than a fanout exchange
    called "exchange", to further simplify the code.

--- test/src/com/rabbitmq/client/test/server/EffectVisibilityCrossNodeTest.java
+++ test/src/com/rabbitmq/client/test/server/EffectVisibilityCrossNodeTest.java
@@ -27,54 +27,30 @@ import java.io.IOException;
  * happen out of order
  */
 public class EffectVisibilityCrossNodeTest extends ClusteredTestBase {
-    private static final String exchange = "exchange";
-
     private String[] queues = new String[QUEUES];
 
     @Override
     protected void createResources() throws IOException {
-        channel.exchangeDeclare(exchange, "fanout");
-
         for (int i = 0; i < queues.length ; i++) {
-            queues[i] = alternateChannel.queueDeclare().getQueue();
-            alternateChannel.queueBind(queues[i], exchange, "");
+            queues[i] = alternateChannel.queueDeclare("", false, false, true, null).getQueue();
+            alternateChannel.queueBind(queues[i], "amq.fanout", "");
         }
-    }
-
-    @Override
-    protected void releaseResources() throws IOException {
-        channel.exchangeDelete(exchange);
     }
 
     private static final int QUEUES = 5;
-    private static final int COMMITS = 500;
-    private static final int MESSAGES_PER_COMMIT = 10;
+    private static final int BATCHES = 500;
+    private static final int MESSAGES_PER_BATCH = 10;
+
+    private static final byte[] msg = "".getBytes();
 
     public void testEffectVisibility() throws Exception {
-        channel.txSelect();
 
-        for (int i = 0; i < COMMITS; i++) {
-            for (int j = 0; j < MESSAGES_PER_COMMIT; j++) {
-                channel.basicPublish(exchange, "", MessageProperties.MINIMAL_BASIC, ("" + (i * MESSAGES_PER_COMMIT + j)).getBytes());
+        for (int i = 0; i < BATCHES; i++) {
+            for (int j = 0; j < MESSAGES_PER_BATCH; j++) {
+                channel.basicPublish("amq.fanout", "", null, msg);
             }
-            channel.txCommit();
-
-            for (int j = 0; j < MESSAGES_PER_COMMIT; j++) {
-                channel.basicPublish(exchange, "", MessageProperties.MINIMAL_BASIC, "bad".getBytes());
-            }
-            channel.txRollback();
-        }
-
-        for (int i = 0; i < queues.length ; i++) {
-            QueueingConsumer consumer = new QueueingConsumer(alternateChannel);
-            alternateChannel.basicConsume(queues[i], true, consumer);
-
-            for (int j = 0; j < MESSAGES_PER_COMMIT * COMMITS; j++) {
-                QueueingConsumer.Delivery delivery = consumer.nextDelivery(1000);
-                assertNotNull(delivery);
-                int sequence = Integer.parseInt(new String(delivery.getBody()));
-
-                assertEquals(j, sequence);
+            for (int j = 0; j < queues.length ; j++) {
+                assertEquals(MESSAGES_PER_BATCH, channel.queuePurge(queues[i]).getMessageCount());
             }
         }
     }
