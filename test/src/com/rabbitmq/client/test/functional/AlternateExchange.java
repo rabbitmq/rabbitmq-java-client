e33008dc74cf9e5af379d058556ec667d10ee5f1
Matthias Radestock <matthias@rabbitmq.com>
2011-06-25 19:25:21 +0100

    get rid of remaining tx tests turns out they all test tx-related functionality, rather than employing tx for some other purpose

--- test/src/com/rabbitmq/client/test/functional/AlternateExchange.java
+++ test/src/com/rabbitmq/client/test/functional/AlternateExchange.java
@@ -111,16 +111,6 @@ public class AlternateExchange extends BrokerTestCase
         }
     }
 
-    protected void publish(String key, boolean mandatory, boolean immediate)
-        throws IOException {
-        channel.basicPublish("x", key, mandatory, immediate, null,
-                             "ae-test".getBytes());
-    }
-
-    protected void publish(String key) throws IOException {
-        publish(key, false, false);
-    }
-
     /**
      * Perform an auto-acking 'basic.get' on each of the queues named
      * in {@link #resources} and check whether a message can be
@@ -161,7 +151,8 @@ public class AlternateExchange extends BrokerTestCase
         throws IOException {
 
         gotReturn.set(false);
-        publish(key, mandatory, immediate);
+        channel.basicPublish("x", key, mandatory, immediate, null,
+                             "ae-test".getBytes());
         checkGet(expected);
         assertEquals(ret, gotReturn.get());
     }
@@ -210,19 +201,6 @@ public class AlternateExchange extends BrokerTestCase
             check(k, true, false, k.equals("z"));
             //immediate
             check(k, false, true, unrouted, true);
-        }
-
-        //tx
-        channel.txSelect();
-        for (String k : keys) {
-            publish(k);
-            checkGet(unrouted);
-            channel.txRollback();
-            checkGet(unrouted);
-            publish(k);
-            checkGet(unrouted);
-            channel.txCommit();
-            checkGet(expected(k));
         }
 
         cleanup();
