6666ab027a2b77566f9e28d5b4fea329a73c2d6f
Steve Powell <steve@rabbitmq.com>
2011-07-25 15:53:42 +0100

    Backout changeset c406a02386385438ae9a0912350a3372b66c146f

--- src/com/rabbitmq/client/impl/ChannelN.java
+++ src/com/rabbitmq/client/impl/ChannelN.java
@@ -162,13 +162,12 @@ public class ChannelN extends AMQChannel implements com.rabbitmq.client.Channel 
     public boolean waitForConfirms()
         throws InterruptedException
     {
-        long seqHead = this.getNextPublishSeqNo();
         synchronized (unconfirmedSet) {
             while (true) {
                 if (getCloseReason() != null) {
                     throw Utility.fixStackTrace(getCloseReason());
                 }
-                if (unconfirmedSet.headSet(seqHead).isEmpty()) {
+                if (unconfirmedSet.isEmpty()) {
                     boolean aux = onlyAcksReceived;
                     onlyAcksReceived = true;
                     return aux;
