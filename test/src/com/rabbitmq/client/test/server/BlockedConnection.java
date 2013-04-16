34f8119856f720c25da7e16fd05f1d982f738e9d
Simon MacMullen <simon@rabbitmq.com>
2013-04-16 13:19:17 +0100

    Inline

--- test/src/com/rabbitmq/client/test/server/BlockedConnection.java
+++ test/src/com/rabbitmq/client/test/server/BlockedConnection.java
@@ -44,31 +44,23 @@ public class BlockedConnection extends BrokerTestCase {
         Host.rabbitmqctl("set_vm_memory_high_watermark 0.4");
     }
 
-    private Connection connection(CountDownLatch latch) throws IOException {
+    private Connection connection(final CountDownLatch latch) throws IOException {
         ConnectionFactory factory = new ConnectionFactory();
         Connection connection = factory.newConnection();
-        connection.addBlockedListener(new Listener(latch));
+        connection.addBlockedListener(new BlockedListener() {
+            public void handleBlocked(String reason) throws IOException {
+                unblock();
+            }
+
+            public void handleUnblocked() throws IOException {
+                latch.countDown();
+            }
+        });
         return connection;
     }
 
     private void publish(Connection connection) throws IOException {
         Channel ch = connection.createChannel();
         ch.basicPublish("", "", MessageProperties.BASIC, "".getBytes());
-    }
-
-    private class Listener implements BlockedListener {
-        private final CountDownLatch latch;
-
-        public Listener(CountDownLatch latch) {
-            this.latch = latch;
-        }
-
-        public void handleBlocked(String reason) throws IOException {
-            unblock();
-        }
-
-        public void handleUnblocked() throws IOException {
-            latch.countDown();
-        }
     }
 }
