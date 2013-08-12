a7bf273168aaafd4018665d76372bc11a88c1ea0
Michael Klishin <michael@rabbitmq.com>
2013-08-12 15:29:02 +0400

    Refer to correctly spelt test case

--- test/src/com/rabbitmq/client/test/functional/FunctionalTests.java
+++ test/src/com/rabbitmq/client/test/functional/FunctionalTests.java
@@ -61,7 +61,7 @@ public class FunctionalTests extends TestCase {
         suite.addTestSuite(DefaultExchange.class);
         suite.addTestSuite(UnbindAutoDeleteExchange.class);
         suite.addTestSuite(Confirm.class);
-        suite.addTestSuite(ConsumerCancelNotificiation.class);
+        suite.addTestSuite(ConsumerCancelNotification.class);
         suite.addTestSuite(UnexpectedFrames.class);
         suite.addTestSuite(PerQueueTTL.class);
         suite.addTestSuite(PerMessageTTL.class);
