a2df6a1fe4c7dd595d57fa3f4cbff6de1d7578d0
Simon MacMullen <simon@rabbitmq.com>
2013-10-01 14:09:30 +0100

    Add a stub to redirect people.

---
+++ test/src/com/rabbitmq/examples/MulticastMain.java
@@ -0,0 +1,34 @@
+//  The contents of this file are subject to the Mozilla Public License
+//  Version 1.1 (the "License"); you may not use this file except in
+//  compliance with the License. You may obtain a copy of the License
+//  at http://www.mozilla.org/MPL/
+//
+//  Software distributed under the License is distributed on an "AS IS"
+//  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+//  the License for the specific language governing rights and
+//  limitations under the License.
+//
+//  The Original Code is RabbitMQ.
+//
+//  The Initial Developer of the Original Code is GoPivotal, Inc.
+//  Copyright (c) 2007-2013 GoPivotal, Inc.  All rights reserved.
+//
+
+package com.rabbitmq.examples;
+
+/**
+ * TODO: delete this after a suitable time has passed
+ */
+public class MulticastMain {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("********************************************************");
+        System.out.println("* NOTE:                                                *");
+        System.out.println("*  com.rabbitmq.examples.MulticastMain is now known as *");
+        System.out.println("*  com.rabbitmq.examples.PerfTest                      *");
+        System.out.println("*                                                      *");
+        System.out.println("********************************************************");
+        System.out.println();
+        System.exit(1);
+    }
+}
