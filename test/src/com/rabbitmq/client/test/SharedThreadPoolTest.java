90579ea294793765715575e6a0f5f6024ac8defa
Michael Klishin <michael@rabbitmq.com>
2013-11-01 19:48:54 +0400

    Refactor tests

--- test/src/com/rabbitmq/client/test/SharedThreadPoolTest.java
+++ test/src/com/rabbitmq/client/test/SharedThreadPoolTest.java
@@ -8,24 +8,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class SharedThreadPoolTest extends BrokerTestCase {
-    public void testSharedThreadPool() throws IOException {
-        ConnectionFactory cf = new ConnectionFactory();
-        ExecutorService executor = Executors.newFixedThreadPool(8);
-        cf.setSharedExecutor(executor);
-
-        AMQConnection conn1 = (AMQConnection)cf.newConnection();
-        assertFalse(conn1.willShutDownConsumerExecutor());
-
-        AMQConnection conn2 = (AMQConnection)cf.newConnection();
-        assertFalse(conn2.willShutDownConsumerExecutor());
-
-        AMQConnection conn3 = (AMQConnection)cf.newConnection(Executors.newSingleThreadExecutor());
-        assertFalse(conn3.willShutDownConsumerExecutor());
-
-        assertSame(conn1.getConsumerExecutor(), conn2.getConsumerExecutor());
-        assertNotSame(conn2.getConsumerExecutor(), conn3.getConsumerExecutor());
-    }
-
     public void testWillShutDownExecutor() throws IOException {
         ConnectionFactory cf = new ConnectionFactory();
         ExecutorService executor = Executors.newFixedThreadPool(8);
@@ -34,13 +16,10 @@ public class SharedThreadPoolTest extends BrokerTestCase {
         AMQConnection conn1 = (AMQConnection)cf.newConnection();
         assertFalse(conn1.willShutDownConsumerExecutor());
 
-        AMQConnection conn2 = (AMQConnection)cf.newConnection();
+        AMQConnection conn2 = (AMQConnection)cf.newConnection(Executors.newSingleThreadExecutor());
         assertFalse(conn2.willShutDownConsumerExecutor());
 
-        AMQConnection conn3 = (AMQConnection)cf.newConnection(Executors.newSingleThreadExecutor());
-        assertFalse(conn3.willShutDownConsumerExecutor());
-
-        AMQConnection conn4 = (AMQConnection)cf.newConnection((ExecutorService)null);
-        assertTrue(conn4.willShutDownConsumerExecutor());
+        AMQConnection conn3 = (AMQConnection)cf.newConnection((ExecutorService)null);
+        assertTrue(conn3.willShutDownConsumerExecutor());
     }
 }
