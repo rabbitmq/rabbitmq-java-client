003816a129499a96834d44f2b54a593b07903717
Tim Watson <watson.timothy@gmail.com>
2013-11-07 12:03:48 +0000

    Backout changeset e27369b5f07c4b089246fb0df6b343398d5d40f4

--- src/com/rabbitmq/client/QueueingConsumer.java
+++ src/com/rabbitmq/client/QueueingConsumer.java
@@ -138,7 +138,9 @@ public class QueueingConsumer extends DefaultConsumer {
     @Override public void handleDelivery(String consumerTag,
                                Envelope envelope,
                                AMQP.BasicProperties properties,
-                               byte[] body) throws IOException {
+                               byte[] body)
+        throws IOException
+    {
         queue.add(new Delivery(consumerTag, envelope, properties, body));
     }
 
@@ -233,9 +235,9 @@ public class QueueingConsumer extends DefaultConsumer {
     * @throws ShutdownSignalException if the connection is shut down while waiting
     * @throws ConsumerCancelledException if this consumer is cancelled while waiting
      */
-    public Delivery nextDelivery() throws InterruptedException,
-                                          ShutdownSignalException,
-                                          ConsumerCancelledException {
+    public Delivery nextDelivery()
+            throws InterruptedException, ShutdownSignalException, ConsumerCancelledException
+    {
         return handle(queue.take());
     }
 
@@ -247,9 +249,9 @@ public class QueueingConsumer extends DefaultConsumer {
      * @throws ShutdownSignalException if the connection is shut down while waiting
      * @throws ConsumerCancelledException if this consumer is cancelled while waiting
      */
-    public Delivery nextDelivery(long timeout) throws InterruptedException,
-                                                      ConsumerCancelledException,
-                                                      ShutdownSignalException {
+    public Delivery nextDelivery(long timeout)
+            throws InterruptedException, ConsumerCancelledException, ShutdownSignalException
+    {
         return handle(queue.poll(timeout, TimeUnit.MILLISECONDS));
     }
 
