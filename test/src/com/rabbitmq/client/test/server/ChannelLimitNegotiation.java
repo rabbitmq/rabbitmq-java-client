3dd0bac3af8baaa64dc7f4b64e7c2ef0c525f2b3
Michael Klishin <michael@rabbitmq.com>
2013-11-29 16:12:10 +0400

    Reformat to 4 spaces

--- test/src/com/rabbitmq/client/test/server/ChannelLimitNegotiation.java
+++ test/src/com/rabbitmq/client/test/server/ChannelLimitNegotiation.java
@@ -10,93 +10,93 @@ import java.io.IOException;
 import java.util.concurrent.Executors;
 
 public class ChannelLimitNegotiation extends BrokerTestCase {
-  class SpecialConnection extends AMQConnection {
-    private final int channelMax;
+    class SpecialConnection extends AMQConnection {
+        private final int channelMax;
 
-    public SpecialConnection(int channelMax) throws Exception {
-      this(new ConnectionFactory(), channelMax);
+        public SpecialConnection(int channelMax) throws Exception {
+            this(new ConnectionFactory(), channelMax);
+        }
+
+        private SpecialConnection(ConnectionFactory factory, int channelMax) throws Exception {
+            super(factory.getUsername(),
+                         factory.getPassword(),
+                         new SocketFrameHandler(SocketFactory.getDefault().createSocket("localhost", AMQP.PROTOCOL.PORT)),
+                         Executors.newFixedThreadPool(1),
+                         factory.getVirtualHost(),
+                         factory.getClientProperties(),
+                         factory.getRequestedFrameMax(),
+                         channelMax,
+                         factory.getRequestedHeartbeat(),
+                         factory.getSaslConfig(),
+                         new DefaultExceptionHandler());
+
+            this.channelMax = channelMax;
+        }
+
+        /**
+         * Private API, allows for easier simulation of bogus clients.
+         */
+        @Override
+        protected int negotiateChannelMax(int requestedChannelMax, int serverMax) {
+            return this.channelMax;
+        }
     }
 
-    private SpecialConnection(ConnectionFactory factory, int channelMax) throws Exception {
-      super(factory.getUsername(),
-          factory.getPassword(),
-          new SocketFrameHandler(SocketFactory.getDefault().createSocket("localhost", AMQP.PROTOCOL.PORT)),
-          Executors.newFixedThreadPool(1),
-          factory.getVirtualHost(),
-          factory.getClientProperties(),
-          factory.getRequestedFrameMax(),
-          channelMax,
-          factory.getRequestedHeartbeat(),
-          factory.getSaslConfig(),
-          new DefaultExceptionHandler());
+    public void testChannelMaxLowerThanServerMinimum() throws Exception {
+        int n = 64;
+        ConnectionFactory cf = new ConnectionFactory();
+        cf.setRequestedChannelMax(n);
 
-      this.channelMax = channelMax;
+        Connection conn = cf.newConnection();
+        assertEquals(n, conn.getChannelMax());
     }
 
-    /**
-     * Private API, allows for easier simulation of bogus clients.
-     */
-    @Override
-    protected int negotiateChannelMax(int requestedChannelMax, int serverMax) {
-      return this.channelMax;
-    }
-  }
-
-  public void testChannelMaxLowerThanServerMinimum() throws Exception {
-    int n = 64;
-    ConnectionFactory cf = new ConnectionFactory();
-    cf.setRequestedChannelMax(n);
-
-    Connection conn = cf.newConnection();
-    assertEquals(n, conn.getChannelMax());
-  }
-
-  public void testChannelMaxGreaterThanServerValue() throws Exception {
-      try {
-        Host.rabbitmqctl("eval 'application:set_env(rabbit, channel_max, 2048).'");
-
-        SpecialConnection connection = new SpecialConnection(4096);
+    public void testChannelMaxGreaterThanServerValue() throws Exception {
         try {
-          connection.start();
-          fail("expected failure during connection negotiation");
-        } catch (IOException e) {
-          // expected
+            Host.rabbitmqctl("eval 'application:set_env(rabbit, channel_max, 2048).'");
+
+            SpecialConnection connection = new SpecialConnection(4096);
+            try {
+                connection.start();
+                fail("expected failure during connection negotiation");
+            } catch (IOException e) {
+                // expected
+            }
+        } finally {
+            Host.rabbitmqctl("eval 'application:set_env(rabbit, channel_max, 0).'");
         }
-      } finally {
-        Host.rabbitmqctl("eval 'application:set_env(rabbit, channel_max, 0).'");
-      }
-  }
-
-  public void testOpeningTooManyChannels() throws Exception {
-    int n = 48;
-
-    try {
-      Host.rabbitmqctl("eval 'application:set_env(rabbit, channel_max, " + n + ").'");
-      ConnectionFactory cf = new ConnectionFactory();
-      Connection conn = cf.newConnection();
-      assertEquals(n, conn.getChannelMax());
-
-      for(int i = 1; i <= n; i++) {
-        assertNotNull(conn.createChannel(i));
-      }
-      // ChannelManager guards against channel.open being sent
-      assertNull(conn.createChannel(n + 1));
-
-      // Construct a channel directly
-      final ChannelN ch = new ChannelN((AMQConnection)conn, n + 1,
-          new ConsumerWorkService(Executors.newSingleThreadExecutor()));
-      conn.addShutdownListener(new ShutdownListener() {
-        public void shutdownCompleted(ShutdownSignalException cause) {
-          // make sure channel.open continuation is released
-          ch.processShutdownSignal(cause, true, true);
-        }
-      });
-      ch.open();
-      fail("expected channel.open to cause a connection exception");
-    } catch (IOException e) {
-      checkShutdownSignal(530, e);
-    } finally {
-      Host.rabbitmqctl("eval 'application:set_env(rabbit, channel_max, 0).'");
     }
-  }
+
+    public void testOpeningTooManyChannels() throws Exception {
+        int n = 48;
+
+        try {
+            Host.rabbitmqctl("eval 'application:set_env(rabbit, channel_max, " + n + ").'");
+            ConnectionFactory cf = new ConnectionFactory();
+            Connection conn = cf.newConnection();
+            assertEquals(n, conn.getChannelMax());
+
+            for (int i = 1; i <= n; i++) {
+                assertNotNull(conn.createChannel(i));
+            }
+            // ChannelManager guards against channel.open being sent
+            assertNull(conn.createChannel(n + 1));
+
+            // Construct a channel directly
+            final ChannelN ch = new ChannelN((AMQConnection) conn, n + 1,
+                                                    new ConsumerWorkService(Executors.newSingleThreadExecutor()));
+            conn.addShutdownListener(new ShutdownListener() {
+                public void shutdownCompleted(ShutdownSignalException cause) {
+                    // make sure channel.open continuation is released
+                    ch.processShutdownSignal(cause, true, true);
+                }
+            });
+            ch.open();
+            fail("expected channel.open to cause a connection exception");
+        } catch (IOException e) {
+            checkShutdownSignal(530, e);
+        } finally {
+            Host.rabbitmqctl("eval 'application:set_env(rabbit, channel_max, 0).'");
+        }
+    }
 }
