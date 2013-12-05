fc2f2a76a4db16b79770022161e95d43aea61859
Simon MacMullen <simon@rabbitmq.com>
2013-12-05 15:57:02 +0000

    Revert (inadvertent?) changes to frame-max tests.

--- test/src/com/rabbitmq/client/test/functional/FrameMax.java
+++ test/src/com/rabbitmq/client/test/functional/FrameMax.java
@@ -34,7 +34,6 @@ import com.rabbitmq.client.impl.Frame;
 import com.rabbitmq.client.impl.FrameHandler;
 import com.rabbitmq.client.impl.LongStringHelper;
 import com.rabbitmq.client.impl.SocketFrameHandler;
-import com.rabbitmq.tools.Host;
 
 public class FrameMax extends BrokerTestCase {
     /* This value for FrameMax is larger than the minimum and less
@@ -74,15 +73,12 @@ public class FrameMax extends BrokerTestCase {
     public void testRejectLargeFramesDuringConnectionNegotiation()
         throws IOException
     {
-        Host.rabbitmqctl("eval 'application:set_env(rabbit, frame_max, " + Integer.toString(AMQP.FRAME_MIN_SIZE) + ").'");
         ConnectionFactory cf = new ConnectionFactory();
         cf.getClientProperties().put("too_long", LongStringHelper.asLongString(new byte[AMQP.FRAME_MIN_SIZE]));
         try {
             cf.newConnection();
             fail("Expected exception during connection negotiation");
         } catch (IOException e) {
-        } finally {
-            Host.rabbitmqctl("eval 'application:set_env(rabbit, frame_max, 131072).'");
         }
     }
 
@@ -93,7 +89,6 @@ public class FrameMax extends BrokerTestCase {
     {
         closeChannel();
         closeConnection();
-
         ConnectionFactory cf = new GenerousConnectionFactory();
         connection = cf.newConnection();
         openChannel();
