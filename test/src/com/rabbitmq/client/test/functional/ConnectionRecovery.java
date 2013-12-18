b25774ad6d2c44d1655e6b16e000471576b996ef
Michael Klishin <michael@rabbitmq.com>
2013-12-18 13:09:35 +0400

    Test channel recovery

--- test/src/com/rabbitmq/client/test/functional/ConnectionRecovery.java
+++ test/src/com/rabbitmq/client/test/functional/ConnectionRecovery.java
@@ -1,5 +1,6 @@
 package com.rabbitmq.client.test.functional;
 
+import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.impl.recovery.RecoveringConnection;
 import com.rabbitmq.tools.Host;
@@ -20,19 +21,42 @@ public class ConnectionRecovery extends TestCase {
         assertTrue(c.isOpen());
         try {
             Host.closeConnection(ci.getPid());
-            expectConnectionRecovery(RECOVERY_INTERVAL, c);
+            expectConnectionRecovery(c);
         } finally {
             c.abort();
         }
     }
 
-    private void expectConnectionRecovery(int recoveryInterval, RecoveringConnection c) throws InterruptedException {
+    public void testChannelRecovery() throws IOException, InterruptedException {
+        ConnectionFactory cf = new ConnectionFactory();
+        cf.setNetworkRecoveryInterval(RECOVERY_INTERVAL);
+        RecoveringConnection c = (RecoveringConnection) cf.newRecoveringConnection();
+        Channel ch = c.createChannel();
+        Host.ConnectionInfo ci = findConnectionInfoFor(Host.listConnections(), c);
+
+        assertTrue(ch.isOpen());
+        try {
+            Host.closeConnection(ci.getPid());
+            expectChannelRecovery(ch);
+        } finally {
+            c.abort();
+        }
+    }
+
+    private void expectConnectionRecovery(RecoveringConnection c) throws InterruptedException {
         String oldName = c.getName();
         Thread.sleep(20);
         assertFalse(c.isOpen());
-        Thread.sleep(recoveryInterval + 100);
+        Thread.sleep(RECOVERY_INTERVAL + 100);
         assertTrue(c.isOpen());
         assertFalse(oldName.equals(c.getName()));
+    }
+
+    private void expectChannelRecovery(Channel ch) throws InterruptedException {
+        Thread.sleep(20);
+        assertFalse(ch.isOpen());
+        Thread.sleep(RECOVERY_INTERVAL + 120);
+        assertTrue(ch.isOpen());
     }
 
     private Host.ConnectionInfo findConnectionInfoFor(List<Host.ConnectionInfo> xs, RecoveringConnection c) {
