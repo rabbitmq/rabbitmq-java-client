d71d52c9bb1db7ce5b9b1344b57ed0b25fa1ea60
Michael Klishin <michael@rabbitmq.com>
2014-01-09 18:32:17 +0400

    Remove unused imports

--- test/src/com/rabbitmq/client/test/functional/ConnectionRecovery.java
+++ test/src/com/rabbitmq/client/test/functional/ConnectionRecovery.java
@@ -11,10 +11,7 @@ import com.rabbitmq.tools.Host;
 import java.io.IOException;
 import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ConnectionRecovery extends BrokerTestCase {
