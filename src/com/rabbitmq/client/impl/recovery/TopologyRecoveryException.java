f4dd133a87dbe42e06846f2b939787002e66d615
Michael Klishin <michael@rabbitmq.com>
2014-01-22 15:38:02 +0400

    Javadoc

--- src/com/rabbitmq/client/impl/recovery/TopologyRecoveryException.java
+++ src/com/rabbitmq/client/impl/recovery/TopologyRecoveryException.java
@@ -1,5 +1,11 @@
 package com.rabbitmq.client.impl.recovery;
 
+/**
+ * Indicates an exception thrown during topology recovery.
+ *
+ * @see com.rabbitmq.client.ConnectionFactory#setTopologyRecovery(boolean)
+ * @since 3.3.0
+ */
 public class TopologyRecoveryException extends Exception {
     public TopologyRecoveryException(String message, Throwable cause) {
         super(message, cause);
