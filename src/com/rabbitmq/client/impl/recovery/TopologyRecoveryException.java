e59e0f69ff7f38decadca39c43a8ee1026a73930
Michael Klishin <michael@rabbitmq.com>
2014-01-22 15:36:32 +0400

    Remove proxy constructors that are not used

--- src/com/rabbitmq/client/impl/recovery/TopologyRecoveryException.java
+++ src/com/rabbitmq/client/impl/recovery/TopologyRecoveryException.java
@@ -1,15 +1,7 @@
 package com.rabbitmq.client.impl.recovery;
 
 public class TopologyRecoveryException extends Exception {
-    public TopologyRecoveryException(Throwable cause) {
-        super(cause);
-    }
-
     public TopologyRecoveryException(String message, Throwable cause) {
         super(message, cause);
-    }
-
-    public TopologyRecoveryException(String message) {
-        super(message);
     }
 }
