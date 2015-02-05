87a28946a33d9fdf92774c998175829038f11ead
Simon MacMullen <simon@rabbitmq.com>
2015-02-05 16:14:49 +0000

    Oops

--- src/com/rabbitmq/client/impl/WorkPool.java
+++ src/com/rabbitmq/client/impl/WorkPool.java
@@ -1,11 +1,9 @@
 package com.rabbitmq.client.impl;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
