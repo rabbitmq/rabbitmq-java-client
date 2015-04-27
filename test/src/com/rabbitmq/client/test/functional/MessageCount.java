0405c61fee3954b53a2d55e79b5320b789c52748
Michael Klishin <mklishin@pivotal.io>
2015-04-27 10:07:16 +0300

    Commit test case for Channel#messageCount

---
+++ test/src/com/rabbitmq/client/test/functional/MessageCount.java
@@ -0,0 +1,21 @@
+package com.rabbitmq.client.test.functional;
+
+import com.rabbitmq.client.test.BrokerTestCase;
+
+import java.io.IOException;
+
+public class MessageCount extends BrokerTestCase {
+    public void testMessageCount() throws IOException {
+        String q = generateQueueName();
+        channel.queueDeclareNoWait(q, false, true, true, null);
+        assertEquals(0, channel.messageCount(q));
+
+        basicPublishVolatile(q);
+        assertEquals(1, channel.messageCount(q));
+        basicPublishVolatile(q);
+        assertEquals(2, channel.messageCount(q));
+
+        channel.queuePurge(q);
+        assertEquals(0, channel.messageCount(q));
+    }
+}
