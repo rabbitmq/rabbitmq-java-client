ee11f5cc7bbcdb362e43bdd4a0084e6160186124
Michael Klishin <mklishin@pivotal.io>
2015-05-13 16:58:50 +0300

    Improve docs around recovery listeners

--- src/com/rabbitmq/client/Recoverable.java
+++ src/com/rabbitmq/client/Recoverable.java
@@ -3,6 +3,13 @@ package com.rabbitmq.client;
 /**
  * Provides a way to register (network, AMQP 0-9-1) connection recovery
  * callbacks.
+ *
+ * When connection recovery is enabled via {@link ConnectionFactory},
+ * {@link ConnectionFactory#newConnection()} and {@link Connection#createChannel()}
+ * return {@link Recoverable} connections and channels.
+ *
+ * @see com.rabbitmq.client.impl.recovery.AutorecoveringConnection
+ * @see com.rabbitmq.client.impl.recovery.AutorecoveringChannel
  */
 public interface Recoverable {
     /**
