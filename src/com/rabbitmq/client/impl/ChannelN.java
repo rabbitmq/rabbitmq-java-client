5786af24595a1383da24db9c1a23da3affabb94d
Michael Klishin <mklishin@pivotal.io>
2015-05-15 14:53:02 +0300

    Silence deprecation warnings in Channel implementations

--- src/com/rabbitmq/client/impl/ChannelN.java
+++ src/com/rabbitmq/client/impl/ChannelN.java
@@ -137,14 +137,20 @@ public class ChannelN extends AMQChannel implements com.rabbitmq.client.Channel 
         returnListeners.clear();
     }
 
+    @SuppressWarnings("deprecation")
+    @Deprecated
     public void addFlowListener(FlowListener listener) {
         flowListeners.add(listener);
     }
 
+    @SuppressWarnings("deprecation")
+    @Deprecated
     public boolean removeFlowListener(FlowListener listener) {
         return flowListeners.remove(listener);
     }
 
+    @SuppressWarnings("deprecation")
+    @Deprecated
     public void clearFlowListeners() {
         flowListeners.clear();
     }
@@ -1166,6 +1172,8 @@ public class ChannelN extends AMQChannel implements com.rabbitmq.client.Channel 
     }
 
     /** Public API - {@inheritDoc} */
+    @SuppressWarnings("deprecation")
+    @Deprecated
     public boolean flowBlocked() {
         return _blockContent;
     }
