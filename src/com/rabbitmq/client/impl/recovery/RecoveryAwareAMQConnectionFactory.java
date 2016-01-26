a642b961c06f5ec37668d7dd8675d588c1c6ff65
Michael Klishin <michael@clojurewerkz.org>
2016-01-27 01:07:51 +0300

    Cosmetics, remove unused code
     * The Address[] constructor is not used by user apps.

--- src/com/rabbitmq/client/impl/recovery/RecoveryAwareAMQConnectionFactory.java
+++ src/com/rabbitmq/client/impl/recovery/RecoveryAwareAMQConnectionFactory.java
@@ -7,52 +7,46 @@ import com.rabbitmq.client.impl.FrameHandlerFactory;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
 
 public class RecoveryAwareAMQConnectionFactory {
-  private final ConnectionParams params;
-  private final FrameHandlerFactory factory;
-  private final List<Address> addrs;
+    private final ConnectionParams params;
+    private final FrameHandlerFactory factory;
+    private final List<Address> addrs;
 
-  public RecoveryAwareAMQConnectionFactory(ConnectionParams params, FrameHandlerFactory factory, Address[] addrs) {
-    this.params = params;
-    this.factory = factory;
-    this.addrs = Arrays.asList(addrs);
-  }
-
-  public RecoveryAwareAMQConnectionFactory(ConnectionParams params, FrameHandlerFactory factory, List<Address> addrs){
-    this.params = params;
-    this.factory = factory;
-    this.addrs = addrs;
-  }
-  /**
-   * @return an interface to the connection
-   * @throws java.io.IOException if it encounters a problem
-   */
-  RecoveryAwareAMQConnection newConnection() throws IOException, TimeoutException {
-    IOException lastException = null;
-    List<Address> shuffled = shuffle(addrs);
-
-    for (Address addr : shuffled) {
-      try {
-        FrameHandler frameHandler = factory.create(addr);
-        RecoveryAwareAMQConnection conn = new RecoveryAwareAMQConnection(params, frameHandler);
-        conn.start();
-        return conn;
-      } catch (IOException e) {
-        lastException = e;
-      }
+    public RecoveryAwareAMQConnectionFactory(ConnectionParams params, FrameHandlerFactory factory, List<Address> addrs) {
+        this.params = params;
+        this.factory = factory;
+        this.addrs = addrs;
     }
 
-    throw (lastException != null) ? lastException : new IOException("failed to connect");
-  }
+    /**
+     * @return an interface to the connection
+     * @throws java.io.IOException if it encounters a problem
+     */
+    RecoveryAwareAMQConnection newConnection() throws IOException, TimeoutException {
+        IOException lastException = null;
+        List<Address> shuffled = shuffle(addrs);
 
-  private List<Address> shuffle(List<Address> addrs) {
-    List<Address> list = new ArrayList<Address>(addrs);
-    Collections.shuffle(list);
-    return list;
-  }
+        for (Address addr : shuffled) {
+            try {
+                FrameHandler frameHandler = factory.create(addr);
+                RecoveryAwareAMQConnection conn = new RecoveryAwareAMQConnection(params, frameHandler);
+                conn.start();
+                return conn;
+            } catch (IOException e) {
+                lastException = e;
+            }
+        }
+
+        throw (lastException != null) ? lastException : new IOException("failed to connect");
+    }
+
+    private List<Address> shuffle(List<Address> addrs) {
+        List<Address> list = new ArrayList<Address>(addrs);
+        Collections.shuffle(list);
+        return list;
+    }
 }
