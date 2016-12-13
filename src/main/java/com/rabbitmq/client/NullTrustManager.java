f8f9a914f3d6d47edb2eae50e12f5908b7eeae97
harrydevnull <hareendran.c.nair@gmail.com>
2016-12-12 21:03:18 -0500

    updated the NullTrustManager to delegate to java default Trustmanager

--- src/main/java/com/rabbitmq/client/NullTrustManager.java
+++ src/main/java/com/rabbitmq/client/NullTrustManager.java
@@ -16,38 +16,126 @@
 
 package com.rabbitmq.client;
 
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.X509TrustManager;
 
 /**
  * Convenience class providing a default implementation of javax.net.ssl.X509TrustManager.
- * Trusts every single certificate presented to it.
+ * Instead doing nothing with the certs delegate to Java's default TrustManager
+ * in case if the server is self-signed we have 2 options
+ * 1. create a new trust store and pass it to connectionfactory
+ * 2. import the root-ca into $JAVA_HOME/jre/lib/security/cacerts
  */
 public class NullTrustManager implements X509TrustManager {
-    /**
-     * Doesn't even bother looking at its arguments, simply returns,
-     * which makes the check succeed.
-     */
-    @Override
-    public void checkClientTrusted(X509Certificate[] chain, String authType) {
-        // Do nothing.
+    private final List<X509TrustManager> trustManagers;
+
+    public NullTrustManager(List<X509TrustManager> trustManagers) {
+        this.trustManagers = Collections.unmodifiableList(trustManagers);
     }
 
-    /**
-     * Doesn't even bother looking at its arguments, simply returns,
-     * which makes the check succeed.
-     */
-    @Override
-    public void checkServerTrusted(X509Certificate[] chain, String authType) {
-        // Do nothing.
+    public NullTrustManager() {
+        this((KeyStore)null);
     }
 
-    /**
-     * Always returns an empty array of X509Certificates.
-     */
+    public NullTrustManager(KeyStore keystore) {
+        List<X509TrustManager> x509TrustManagers = new ArrayList<X509TrustManager>();
+        x509TrustManagers.add(getDefaultTrustManager());
+        x509TrustManagers.add(getTrustManager(keystore));
+
+        this.trustManagers = Collections.unmodifiableList(x509TrustManagers);
+
+    }
+
+    @Override
+    public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+        for (X509TrustManager trustManager : trustManagers) {
+            try {
+                trustManager.checkClientTrusted(chain, authType);
+                return; // someone trusts them. success!
+            } catch (CertificateException e) {
+                // maybe someone else will trust them
+            }
+        }
+        throw new CertificateException("None of the TrustManagers trust this certificate chain");
+    }
+
+    @Override
+    public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+        for (X509TrustManager trustManager : trustManagers) {
+            try {
+                trustManager.checkServerTrusted(chain, authType);
+                return; // someone trusts them. success!
+            } catch (CertificateException e) {
+                // maybe someone else will trust them
+            }
+        }
+        throw new CertificateException("None of the TrustManagers trust this certificate chain");
+    }
+
     @Override
     public X509Certificate[] getAcceptedIssuers() {
-        return new X509Certificate[0];
+        List<X509Certificate> certificates = new ArrayList<X509Certificate>();
+        for (X509TrustManager trustManager : trustManagers) {
+            for (X509Certificate cert : trustManager.getAcceptedIssuers()) {
+                certificates.add(cert);
+            }
+        }
+        return (X509Certificate[])certificates.toArray();
     }
+
+    public static TrustManager[] getTrustManagers(KeyStore keyStore) {
+
+        return new TrustManager[] { new NullTrustManager(keyStore) };
+
+    }
+
+    public static X509TrustManager getDefaultTrustManager() {
+
+        return getTrustManager(null);
+
+    }
+
+    public static X509TrustManager getTrustManager(KeyStore keystore) {
+
+        return getTrustManager(TrustManagerFactory.getDefaultAlgorithm(), keystore);
+
+    }
+
+    public static X509TrustManager getTrustManager(String algorithm, KeyStore keystore) {
+
+        TrustManagerFactory factory;
+        X509TrustManager x509TrustManager = null;
+
+        try {
+            factory = TrustManagerFactory.getInstance(algorithm);
+            factory.init(keystore);
+            for (TrustManager trustManager : factory.getTrustManagers()) {
+
+                if (trustManager instanceof X509TrustManager) {
+                     x509TrustManager = (X509TrustManager)trustManager;
+                    break;
+                }
+            }
+
+        } catch (NoSuchAlgorithmException  e) {
+            e.printStackTrace();
+        }catch ( KeyStoreException e){
+            e.printStackTrace();
+        }
+
+        return x509TrustManager;
+
+    }
+
 }
