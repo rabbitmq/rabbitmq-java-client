4e282fe02ff0b33cf1cf8094c41804cbfc576a0a
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2017-11-20 13:33:27 +0100

    Remove ByteBufferInputStream tests
    Class does not longer exist.
    
    References #319

--- src/test/java/com/rabbitmq/client/test/FrameTest.java
+++ src/test/java/com/rabbitmq/client/test/FrameTest.java
@@ -2,90 +2,28 @@ package com.rabbitmq.client.test;
 
 import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.impl.Frame;
-import com.rabbitmq.client.impl.nio.ByteBufferInputStream;
 import com.rabbitmq.client.impl.nio.ByteBufferOutputStream;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.channels.WritableByteChannel;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
 
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 /**
  *
  */
 public class FrameTest {
-
-    @Test public void readFrames() throws IOException {
-        Random random = new Random();
-        int nbOfFrames = 100;
-        AccumulatorReadableByteChannel channel = new AccumulatorReadableByteChannel();
-
-        for(int i = 0; i < nbOfFrames; i++) {
-            byte[] payload = new byte[random.nextInt(2000) + 1];
-            Frame frame = new Frame(AMQP.FRAME_METHOD, 1, payload);
-            channel.add(frame);
-        }
-
-        ByteBuffer buffer = ByteBuffer.allocate(8192);
-
-        DataInputStream inputStream = new DataInputStream(
-            new ByteBufferInputStream(channel, buffer)
-        );
-
-        int nbReadFrames = 0;
-        channel.read(buffer);
-        buffer.flip();
-        while(buffer.hasRemaining()) {
-            Frame.readFrom(inputStream);
-            nbReadFrames++;
-            if(!buffer.hasRemaining()) {
-                buffer.clear();
-                channel.read(buffer);
-                buffer.flip();
-            }
-
-        }
-        assertThat(nbReadFrames, equalTo(nbOfFrames));
-    }
-
-    @Test public void readLargeFrame() throws IOException {
-        AccumulatorReadableByteChannel channel = new AccumulatorReadableByteChannel();
-
-        int [] framesSize = new int [] {100, 75, 20000, 150};
-        for (int frameSize : framesSize) {
-            Frame frame = new Frame(AMQP.FRAME_METHOD, 1, new byte[frameSize]);
-            channel.add(frame);
-        }
-
-        ByteBuffer buffer = ByteBuffer.allocate(8192);
-
-        DataInputStream inputStream = new DataInputStream(
-            new ByteBufferInputStream(channel, buffer)
-        );
-
-        int nbReadFrames = 0;
-        channel.read(buffer);
-        buffer.flip();
-        while(buffer.hasRemaining()) {
-            Frame.readFrom(inputStream);
-            nbReadFrames++;
-            if(!buffer.hasRemaining()) {
-                buffer.clear();
-                channel.read(buffer);
-                buffer.flip();
-            }
-
-        }
-        assertThat(nbReadFrames, equalTo(framesSize.length));
-    }
 
     @Test
     public void writeFrames() throws IOException {
