1961244f20351856c84939df226d61454a08f4a0
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2017-12-19 10:08:25 +0100

    Fix server version parsing in test

--- src/test/java/com/rabbitmq/client/test/TestUtils.java
+++ src/test/java/com/rabbitmq/client/test/TestUtils.java
@@ -54,6 +54,10 @@ public class TestUtils {
         if (currentVersion.contains("~")) {
             currentVersion = currentVersion.substring(0, currentVersion.indexOf("~"));
         }
+        // alpha (snapshot) versions: 3.7.1-alpha.40
+        if (currentVersion.contains("-")) {
+            currentVersion = currentVersion.substring(0, currentVersion.indexOf("-"));
+        }
         return "0.0.0".equals(currentVersion) || versionCompare(currentVersion, "3.7.0") >= 0;
     }
 
