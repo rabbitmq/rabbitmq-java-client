4277aeed699647d8bb033f791e03cf043ba07842
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2018-08-27 10:25:01 +0200

    Fix test method name
    References #400
    
    (cherry picked from commit 02164d71c33db72649aeb1c646f57b8fbfda00ed)
    
    Conflicts:
    	src/test/java/com/rabbitmq/client/test/ConnectionFactoryTest.java

--- src/test/java/com/rabbitmq/client/test/ConnectionFactoryTest.java
+++ src/test/java/com/rabbitmq/client/test/ConnectionFactoryTest.java
@@ -124,7 +124,7 @@ public class ConnectionFactoryTest {
         assertThat(addressResolver.get(), allOf(notNullValue(), instanceOf(DnsRecordIpAddressResolver.class)));
     }
 
-    @Test public void shouldNotUseDnsResolutionWhenOneAddressAndNoTls() throws Exception {
+    @Test public void shouldNotUseDnsResolutionWhenOneAddressAndTls() throws Exception {
         final AMQConnection connection = mock(AMQConnection.class);
         final AtomicReference<AddressResolver> addressResolver = new AtomicReference<AddressResolver>();
 
