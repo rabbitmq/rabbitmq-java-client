9980c7a50d73fc8b1bcfa536e253ae8ffd5a7bc5
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2018-10-22 16:09:03 +0200

    Add Javadoc for v4.9.0

--- api/4.x.x/package-search-index.js
+++ api/4.x.x/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"com.rabbitmq.client"},{"l":"com.rabbitmq.client.impl"},{"l":"com.rabbitmq.client.impl.nio"},{"l":"com.rabbitmq.client.impl.recovery"},{"l":"com.rabbitmq.tools.json"},{"l":"com.rabbitmq.tools.jsonrpc"},{"l":"com.rabbitmq.utility"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.rabbitmq.client"},{"l":"com.rabbitmq.client.impl"},{"l":"com.rabbitmq.client.impl.nio"},{"l":"com.rabbitmq.client.impl.recovery"},{"l":"com.rabbitmq.tools.json"},{"l":"com.rabbitmq.tools.jsonrpc"},{"l":"com.rabbitmq.utility"}]
