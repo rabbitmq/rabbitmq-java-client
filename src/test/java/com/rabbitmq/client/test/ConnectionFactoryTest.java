94a93a6bb097703f5817725ad76af740419b8ab5
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2018-11-28 16:42:29 +0100

    Fix test to run without broker in the background
    (cherry picked from commit c4ec981835267e6d9ea6b804b0b5a400e9f56735)

--- src/test/java/com/rabbitmq/client/test/ConnectionFactoryTest.java
+++ src/test/java/com/rabbitmq/client/test/ConnectionFactoryTest.java
@@ -62,7 +62,7 @@ public class ConnectionFactoryTest {
             }
 
             @Override
-            protected synchronized FrameHandlerFactory createFrameHandlerFactory() throws IOException {
+            protected synchronized FrameHandlerFactory createFrameHandlerFactory() {
                 return mock(FrameHandlerFactory.class);
             }
         };
@@ -88,6 +88,11 @@ public class ConnectionFactoryTest {
                 assertSame(provider, params.getCredentialsProvider());
                 createCalled.set(true);
                 return connection;
+            }
+
+            @Override
+            protected synchronized FrameHandlerFactory createFrameHandlerFactory() {
+                return mock(FrameHandlerFactory.class);
             }
         };
         connectionFactory.setCredentialsProvider(provider);
@@ -116,7 +121,14 @@ public class ConnectionFactoryTest {
                 addressResolver.set(super.createAddressResolver(addresses));
                 return addressResolver.get();
             }
+
+            @Override
+            protected synchronized FrameHandlerFactory createFrameHandlerFactory() {
+                return mock(FrameHandlerFactory.class);
+            }
         };
+        // connection recovery makes the creation path more complex
+        connectionFactory.setAutomaticRecoveryEnabled(false);
 
         doNothing().when(connection).start();
         connectionFactory.newConnection();
@@ -140,7 +152,14 @@ public class ConnectionFactoryTest {
                 addressResolver.set(super.createAddressResolver(addresses));
                 return addressResolver.get();
             }
+
+            @Override
+            protected synchronized FrameHandlerFactory createFrameHandlerFactory() {
+                return mock(FrameHandlerFactory.class);
+            }
         };
+        // connection recovery makes the creation path more complex
+        connectionFactory.setAutomaticRecoveryEnabled(false);
 
         doNothing().when(connection).start();
         connectionFactory.useSslProtocol();
