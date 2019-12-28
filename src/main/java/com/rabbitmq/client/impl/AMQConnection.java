5e4eeb03c6f771a367dd747911d232e34615df91
Michael Klishin <michael@clojurewerkz.org>
2019-12-28 19:35:38 +0300

    (c) bump
    (cherry picked from commit 24400f9d06142dd2586bf71964c4036118235e29)
    
    Conflicts:
    	src/main/java/com/rabbitmq/client/impl/AMQConnection.java
    	src/main/java/com/rabbitmq/client/impl/CredentialsProvider.java
    	src/main/java/com/rabbitmq/client/impl/DefaultCredentialsProvider.java
    	src/test/java/com/rabbitmq/client/test/TestUtils.java
    	src/test/java/com/rabbitmq/client/test/functional/Metrics.java

--- src/main/java/com/rabbitmq/client/impl/AMQConnection.java
+++ src/main/java/com/rabbitmq/client/impl/AMQConnection.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2007-Present Pivotal Software, Inc.  All rights reserved.
+// Copyright (c) 2007-2020 Pivotal Software, Inc.  All rights reserved.
 //
 // This software, the RabbitMQ Java client library, is triple-licensed under the
 // Mozilla Public License 1.1 ("MPL"), the GNU General Public License version 2
@@ -35,7 +35,7 @@ import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 final class Copyright {
-    final static String COPYRIGHT="Copyright (c) 2007-2019 Pivotal Software, Inc.";
+    final static String COPYRIGHT="Copyright (c) 2007-2020 Pivotal Software, Inc.";
     final static String LICENSE="Licensed under the MPL. See https://www.rabbitmq.com/";
 }
 
