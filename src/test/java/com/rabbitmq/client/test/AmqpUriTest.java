4ffb6eaf3086635502f14fe13843a462faf84cad
Julien Blondeau <julien_blondeau@ultimatesoftware.com>
2021-02-19 17:09:40 +0100

    Handle basic query parameters in connection URI

--- src/test/java/com/rabbitmq/client/test/AmqpUriTest.java
+++ src/test/java/com/rabbitmq/client/test/AmqpUriTest.java
@@ -32,35 +32,63 @@ public class AmqpUriTest extends BrokerTestCase
     {
         /* From the spec (subset of the tests) */
         parseSuccess("amqp://user:pass@host:10000/vhost",
-                     "user", "pass", "host", 10000, "vhost");
+                     "user", "pass", "host", 10000, "vhost", false);
         parseSuccess("aMQps://user%61:%61pass@host:10000/v%2fhost",
-                     "usera", "apass", "host", 10000, "v/host");
-        parseSuccess("amqp://host", "guest", "guest", "host", 5672, "/");
+                     "usera", "apass", "host", 10000, "v/host", true);
+        parseSuccess("amqp://host", "guest", "guest", "host", 5672, "/", false);
         parseSuccess("amqp:///vhost",
-                     "guest", "guest", "localhost", 5672, "vhost");
-        parseSuccess("amqp://host/", "guest", "guest", "host", 5672, "");
-        parseSuccess("amqp://host/%2f", "guest", "guest", "host", 5672, "/");
-        parseSuccess("amqp://[::1]", "guest", "guest", "[::1]", 5672, "/");
+                     "guest", "guest", "localhost", 5672, "vhost", false);
+        parseSuccess("amqp://host/", "guest", "guest", "host", 5672, "", false);
+        parseSuccess("amqp://host/%2f", "guest", "guest", "host", 5672, "/", false);
+        parseSuccess("amqp://[::1]", "guest", "guest", "[::1]", 5672, "/", false);
 
         /* Various other success cases */
-        parseSuccess("amqp://host:100", "guest", "guest", "host", 100, "/");
-        parseSuccess("amqp://[::1]:100", "guest", "guest", "[::1]", 100, "/");
+        parseSuccess("amqp://host:100", "guest", "guest", "host", 100, "/", false);
+        parseSuccess("amqp://[::1]:100", "guest", "guest", "[::1]", 100, "/", false);
 
         parseSuccess("amqp://host/blah",
-                     "guest", "guest", "host", 5672, "blah");
+                     "guest", "guest", "host", 5672, "blah", false);
         parseSuccess("amqp://host:100/blah",
-                     "guest", "guest", "host", 100, "blah");
+                     "guest", "guest", "host", 100, "blah", false);
         parseSuccess("amqp://[::1]/blah",
-                     "guest", "guest", "[::1]", 5672, "blah");
+                     "guest", "guest", "[::1]", 5672, "blah", false);
         parseSuccess("amqp://[::1]:100/blah",
-                     "guest", "guest", "[::1]", 100, "blah");
+                     "guest", "guest", "[::1]", 100, "blah", false);
 
         parseSuccess("amqp://user:pass@host",
-                     "user", "pass", "host", 5672, "/");
+                     "user", "pass", "host", 5672, "/", false);
         parseSuccess("amqp://user:pass@[::1]",
-                     "user", "pass", "[::1]", 5672, "/");
+                     "user", "pass", "[::1]", 5672, "/", false);
         parseSuccess("amqp://user:pass@[::1]:100",
-                     "user", "pass", "[::1]", 100, "/");
+                     "user", "pass", "[::1]", 100, "/", false);
+
+        /* using query parameters */
+        parseSuccess("amqp://user:pass@host:10000/vhost?",
+                     "user", "pass", "host", 10000, "vhost", false);
+        parseSuccess("amqp://user:pass@host:10000/vhost?&",
+                     "user", "pass", "host", 10000, "vhost", false);
+        parseSuccess("amqp://user:pass@host:10000/vhost?unknown_parameter",
+                     "user", "pass", "host", 10000, "vhost", false);
+        parseSuccess("amqp://user:pass@host:10000/vhost?unknown_parameter=value",
+                     "user", "pass", "host", 10000, "vhost", false);
+        parseSuccess("amqp://user:pass@host:10000/vhost?unknown%2fparameter=value",
+                     "user", "pass", "host", 10000, "vhost", false);
+
+        parseSuccess("amqp://user:pass@host:10000/vhost?heartbeat=342",
+                     "user", "pass", "host", 10000, "vhost", false,
+                     342, null, null);
+        parseSuccess("amqp://user:pass@host:10000/vhost?connection_timeout=442",
+                     "user", "pass", "host", 10000, "vhost", false,
+                     null, 442, null);
+        parseSuccess("amqp://user:pass@host:10000/vhost?channel_max=542",
+                     "user", "pass", "host", 10000, "vhost", false,
+                     null, null, 542);
+        parseSuccess("amqp://user:pass@host:10000/vhost?heartbeat=342&connection_timeout=442&channel_max=542",
+                     "user", "pass", "host", 10000, "vhost", false,
+                     342, 442, 542);
+        parseSuccess("amqp://user:pass@host:10000/vhost?heartbeat=342&connection_timeout=442&channel_max=542&a=b",
+                     "user", "pass", "host", 10000, "vhost", false,
+                     342, 442, 542);
 
         /* Various failure cases */
         parseFail("https://www.rabbitmq.com");
@@ -71,10 +99,26 @@ public class AmqpUriTest extends BrokerTestCase
         parseFail("amqp://foo%1");
         parseFail("amqp://foo%1x");
         parseFail("amqp://foo%xy");
+
+        parseFail("amqp://user:pass@host:10000/vhost?heartbeat=not_an_integer");
+        parseFail("amqp://user:pass@host:10000/vhost?heartbeat=-1");
+        parseFail("amqp://user:pass@host:10000/vhost?connection_timeout=not_an_integer");
+        parseFail("amqp://user:pass@host:10000/vhost?connection_timeout=-1");
+        parseFail("amqp://user:pass@host:10000/vhost?channel_max=not_an_integer");
+        parseFail("amqp://user:pass@host:10000/vhost?channel_max=-1");
+        parseFail("amqp://user:pass@host:10000/vhost?heartbeat=342?connection_timeout=442");
     }
 
     private void parseSuccess(String uri, String user, String password,
-                              String host, int port, String vhost)
+                              String host, int port, String vhost, boolean secured)
+        throws URISyntaxException, NoSuchAlgorithmException, KeyManagementException
+    {
+        parseSuccess(uri, user, password, host, port, vhost, secured, null, null, null);
+    }
+
+    private void parseSuccess(String uri, String user, String password,
+                              String host, int port, String vhost, boolean secured,
+                              Integer heartbeat, Integer connectionTimeout, Integer channelMax)
         throws URISyntaxException, NoSuchAlgorithmException, KeyManagementException
     {
         ConnectionFactory cf = TestUtils.connectionFactory();
@@ -85,6 +129,17 @@ public class AmqpUriTest extends BrokerTestCase
         assertEquals(host, cf.getHost());
         assertEquals(port, cf.getPort());
         assertEquals(vhost, cf.getVirtualHost());
+        assertEquals(secured, cf.isSSL());
+        
+        if(heartbeat != null) {
+            assertEquals(heartbeat.intValue(), cf.getRequestedHeartbeat());
+        }
+        if(connectionTimeout != null) {
+            assertEquals(connectionTimeout.intValue(), cf.getConnectionTimeout());
+        }
+        if(channelMax != null) {
+            assertEquals(channelMax.intValue(), cf.getRequestedChannelMax());
+        }
     }
 
     private void parseFail(String uri) {
