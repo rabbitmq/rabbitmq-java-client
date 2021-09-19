9f012ecca9832fb031dd5182f52485ba01aa630e
Kamila <kamila.albuquerque@ccc.ufcg.edu.br>
2021-09-18 21:24:30 -0300

    UnnecessaryImport

--- src/main/java/com/rabbitmq/client/RecoverableChannel.java
+++ src/main/java/com/rabbitmq/client/RecoverableChannel.java
@@ -1,7 +1,5 @@
 package com.rabbitmq.client;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Recoverable;
 
 /**
  * Convenient interface when working against auto-recovery channels.
