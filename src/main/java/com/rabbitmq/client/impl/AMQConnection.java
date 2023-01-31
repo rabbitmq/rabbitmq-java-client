20e641e1b53dec785b4da9b5f65fb6423b5d16aa
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2023-01-31 14:41:43 +0100

    Fix NPE in metrics collector if channel is null
    Fixes #944

--- src/main/java/com/rabbitmq/client/impl/AMQConnection.java
+++ src/main/java/com/rabbitmq/client/impl/AMQConnection.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2007-2020 VMware, Inc. or its affiliates.  All rights reserved.
+// Copyright (c) 2007-2023 VMware, Inc. or its affiliates.  All rights reserved.
 //
 // This software, the RabbitMQ Java client library, is triple-licensed under the
 // Mozilla Public License 2.0 ("MPL"), the GNU General Public License version 2
@@ -596,7 +596,9 @@ public class AMQConnection extends ShutdownNotifierComponent implements Connecti
         ChannelManager cm = _channelManager;
         if (cm == null) return null;
         Channel channel = cm.createChannel(this, channelNumber);
-        metricsCollector.newChannel(channel);
+        if (channel != null) {
+            metricsCollector.newChannel(channel);
+        }
         return channel;
     }
 
@@ -607,7 +609,9 @@ public class AMQConnection extends ShutdownNotifierComponent implements Connecti
         ChannelManager cm = _channelManager;
         if (cm == null) return null;
         Channel channel = cm.createChannel(this);
-        metricsCollector.newChannel(channel);
+        if (channel != null) {
+            metricsCollector.newChannel(channel);
+        }
         return channel;
     }
 
