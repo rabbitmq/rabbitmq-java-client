04f1801ae6eaac10af7bf802c8fb7065284624e6
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2023-06-15 11:49:56 +0200

    Tweak error message
    References #1062
    
    (cherry picked from commit 5982cd1eb307004fe53353a01da03332aa342eb2)

--- src/main/java/com/rabbitmq/client/impl/Frame.java
+++ src/main/java/com/rabbitmq/client/impl/Frame.java
@@ -111,7 +111,9 @@ public class Frame {
         int payloadSize = is.readInt();
         if (payloadSize >= maxPayloadSize) {
             throw new IllegalStateException(format(
-                "Frame body is too large (%d), maximum size is %d",
+                "Frame body is too large (%d), maximum configured size is %d. " +
+                    "See ConnectionFactory#setMaxInboundMessageBodySize " +
+                    "if you need to increase the limit.",
                 payloadSize, maxPayloadSize
             ));
         }
