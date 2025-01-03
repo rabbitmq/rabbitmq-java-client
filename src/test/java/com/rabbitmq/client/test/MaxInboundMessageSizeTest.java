be27b603f804d24a293013298b84307227f263b6
Michael Klishin <michaelklishin@icloud.com>
2025-01-03 15:47:00 -0500

    Update the copy message advertised to RabbitMQ

--- src/test/java/com/rabbitmq/client/test/MaxInboundMessageSizeTest.java
+++ src/test/java/com/rabbitmq/client/test/MaxInboundMessageSizeTest.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2023-2024 Broadcom. All Rights Reserved. The term "Broadcom" refers to Broadcom
+// Copyright (c) 2023-2025 Broadcom. All Rights Reserved. The term "Broadcom" refers to Broadcom
 // Inc.
 // and/or its subsidiaries.
 //
