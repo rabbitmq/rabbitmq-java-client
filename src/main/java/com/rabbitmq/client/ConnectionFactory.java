86c602999aa8c494237260c14f58ece7df83bf18
Arnaud Cogoluègnes <514737+acogoluegnes@users.noreply.github.com>
2025-08-07 08:52:02 +0200

    Set port to default when URI uses amqp (plain)
    (cherry picked from commit 6748f530e7458c48b96db771f2feade04d7d177f)
    
    Conflicts:
    	src/main/java/com/rabbitmq/client/ConnectionFactory.java

--- src/main/java/com/rabbitmq/client/ConnectionFactory.java
+++ src/main/java/com/rabbitmq/client/ConnectionFactory.java
@@ -350,7 +350,7 @@ public class ConnectionFactory implements Cloneable {
   public void setUri(URI uri)
       throws URISyntaxException, NoSuchAlgorithmException, KeyManagementException {
     if ("amqp".equals(uri.getScheme().toLowerCase())) {
-      // nothing special to do
+      setPort(DEFAULT_AMQP_PORT);
     } else if ("amqps".equals(uri.getScheme().toLowerCase())) {
       setPort(DEFAULT_AMQP_OVER_SSL_PORT);
       // SSL context factory not set yet, we use the default one
@@ -1442,6 +1442,7 @@ public class ConnectionFactory implements Cloneable {
    *
    * @return the connection factory
    * @see #netty()
+   * @since 5.27.0
    */
   public ConnectionFactory useNetty() {
     this.netty = true;
