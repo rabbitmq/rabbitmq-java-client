eb32431524cf286c699b59275a5488fcc1e95ec5
Arnaud Cogoluègnes <514737+acogoluegnes@users.noreply.github.com>
2025-10-21 09:28:03 +0200

    Use different queue and exchange names between tests
    Avoid conflicts between tests in case the record is not completely gone.

--- src/test/java/com/rabbitmq/client/test/functional/DurableOnTransient.java
+++ src/test/java/com/rabbitmq/client/test/functional/DurableOnTransient.java
@@ -1,4 +1,5 @@
-// Copyright (c) 2007-2025 Broadcom. All Rights Reserved. The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+// Copyright (c) 2007-2025 Broadcom. All Rights Reserved.
+// The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
 //
 // This software, the RabbitMQ Java client library, is triple-licensed under the
 // Mozilla Public License 2.0 ("MPL"), the GNU General Public License version 2
@@ -12,89 +13,81 @@
 //
 // If you have any questions regarding licensing, please contact us at
 // info@rabbitmq.com.
-
-
 package com.rabbitmq.client.test.functional;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import java.io.IOException;
-
-import org.junit.jupiter.api.Test;
-
 import com.rabbitmq.client.GetResponse;
 import com.rabbitmq.client.MessageProperties;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
 
-public class DurableOnTransient extends ClusteredTestBase
-{
-    protected static final String Q = "SemiDurableBindings.DurableQueue";
-    protected static final String X = "SemiDurableBindings.TransientExchange";
+public class DurableOnTransient extends ClusteredTestBase {
+  private String q;
+  private String x;
 
-    private GetResponse basicGet()
-        throws IOException
-    {
-        return channel.basicGet(Q, true);
-    }
+  private GetResponse basicGet() throws IOException {
+    return channel.basicGet(q, true);
+  }
 
-    private void basicPublish()
-        throws IOException
-    {
-        channel.basicPublish(X, "",
-                             MessageProperties.PERSISTENT_TEXT_PLAIN,
-                             "persistent message".getBytes());
-    }
+  private void basicPublish() throws IOException {
+    channel.basicPublish(
+        x, "", MessageProperties.PERSISTENT_TEXT_PLAIN, "persistent message".getBytes());
+  }
 
-    protected void createResources() throws IOException {
-        channel.exchangeDelete(X);
-        // transient exchange
-        channel.exchangeDeclare(X, "direct", false);
+  protected void createResources() throws IOException {
+    x = generateExchangeName();
+    q = generateQueueName();
+    channel.exchangeDelete(x);
+    // transient exchange
+    channel.exchangeDeclare(x, "direct", false);
 
-        channel.queueDelete(Q);
-        // durable queue
-        channel.queueDeclare(Q, true, false, false, null);
-    }
+    channel.queueDelete(q);
+    // durable queue
+    channel.queueDeclare(q, true, false, false, null);
+  }
 
-    protected void releaseResources() throws IOException {
-        channel.queueDelete(Q);
-        channel.exchangeDelete(X);
-    }
+  protected void releaseResources() throws IOException {
+    channel.queueDelete(q);
+    channel.exchangeDelete(x);
+  }
 
-    @Test public void bindDurableToTransient()
-        throws IOException
-    {
-        channel.queueBind(Q, X, "");
-        basicPublish();
-        assertNotNull(basicGet());
-    }
+  @Test
+  public void bindDurableToTransient() throws IOException {
+    channel.queueBind(q, x, "");
+    basicPublish();
+    assertNotNull(basicGet());
+  }
 
-    @Test public void semiDurableBindingRemoval() throws IOException {
-        if (clusteredConnection != null) {
-            deleteExchange("x");
-            declareTransientTopicExchange("x");
-            clusteredChannel.queueDelete("q");
-            clusteredChannel.queueDeclare("q", true, false, false, null);
-            channel.queueBind("q", "x", "k");
+  @Test
+  public void semiDurableBindingRemoval() throws IOException {
+    if (clusteredConnection != null) {
+      String exchange = generateExchangeName();
+      String queue = generateQueueName();
+      declareTransientTopicExchange(exchange);
+      clusteredChannel.queueDeclare(queue, true, false, false, null);
+      channel.queueBind(queue, exchange, "k");
 
-            stopSecondary();
-            boolean restarted = false;
-            try {
-                deleteExchange("x");
+      stopSecondary();
+      boolean restarted = false;
+      try {
+        deleteExchange(exchange);
 
-                startSecondary();
-                restarted = true;
+        startSecondary();
+        restarted = true;
 
-                declareTransientTopicExchange("x");
+        declareTransientTopicExchange(exchange);
 
-                basicPublishVolatile("x", "k");
-                assertDelivered("q", 0);
+        basicPublishVolatile(exchange, "k");
+        assertDelivered(queue, 0);
 
-                deleteQueue("q");
-                deleteExchange("x");
-            } finally {
-                if (!restarted) {
-                    startSecondary();
-                }
-            }
+        deleteQueue(queue);
+        deleteExchange(exchange);
+      } finally {
+        if (!restarted) {
+          startSecondary();
         }
+      }
     }
+  }
 }
